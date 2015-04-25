32ddca87ce47e0d49158c2a393413865976c1418
Brian Farnhill <brian@brianfarnhill.com>
2015-04-25 18:43:20 +1000

    Added cache account tests, fixed BCS tests

--- Tests/xSharePoint/xSharePoint.xSPBCSServiceApp.Tests.ps1
+++ Tests/xSharePoint/xSharePoint.xSPBCSServiceApp.Tests.ps1
@@ -11,40 +11,42 @@ Set-StrictMode -Version latest
 
 $RepoRoot = (Resolve-Path $PSScriptRoot\..\..).Path
 
-Import-Module (Join-Path $RepoRoot "Modules\xSharePoint\DSCResources\MSFT_xSPBCSServiceApp\MSFT_xSPBCSServiceApp.psm1")
+$ModuleName = "MSFT_xSPBCSServiceApp"
+Import-Module (Join-Path $RepoRoot "Modules\xSharePoint\DSCResources\$ModuleName\$ModuleName.psm1")
 
 Describe "xSPBCSServiceApp" {
-
-    $testParams = @{
-        Name = "Test App"
-        ApplicationPool = "Test App Pool"
-        DatabaseName = "Test_DB"
-        DatabaseServer = "TestServer\Instance"
-        InstallAccount = New-Object System.Management.Automation.PSCredential ("username", (ConvertTo-SecureString "password" -AsPlainText -Force))
-    }
-
-    Context "Validate test method" {
-        It "Fails when no service app exists" {
-            Mock Get-TargetResource { return @{} } -ModuleName MSFT_xSPBCSServiceApp
-            Test-TargetResource @testParams | Should Be $false
+    InModuleScope $ModuleName {
+        $testParams = @{
+            Name = "Test App"
+            ApplicationPool = "Test App Pool"
+            DatabaseName = "Test_DB"
+            DatabaseServer = "TestServer\Instance"
+            InstallAccount = New-Object System.Management.Automation.PSCredential ("username", (ConvertTo-SecureString "password" -AsPlainText -Force))
         }
-        It "Passes when the service app exists" {
-            Mock Get-TargetResource { 
-                return @{ 
-                    Name = $testParams.Name 
-                    ApplicationPool = $testParams.ApplicationPool
+
+        Context "Validate test method" {
+            It "Fails when no service app exists" {
+                Mock Get-TargetResource { return @{} }
+                Test-TargetResource @testParams | Should Be $false
+            }
+            It "Passes when the service app exists" {
+                Mock Get-TargetResource { 
+                    return @{ 
+                        Name = $testParams.Name 
+                        ApplicationPool = $testParams.ApplicationPool
+                    } 
                 } 
-            } -ModuleName MSFT_xSPBCSServiceApp
-            Test-TargetResource @testParams | Should Be $false
-        }
-        It "Fails when the service app exists but has the wrong app pool" {
-            Mock Get-TargetResource { 
-                return @{ 
-                    Name = $testParams.Name 
-                    ApplicationPool = "Wrong app pool"
-                } 
-            } -ModuleName MSFT_xSPBCSServiceApp
-            Test-TargetResource @testParams | Should Be $false
+                Test-TargetResource @testParams | Should Be $true
+            }
+            It "Fails when the service app exists but has the wrong app pool" {
+                Mock Get-TargetResource { 
+                    return @{ 
+                        Name = $testParams.Name 
+                        ApplicationPool = "Wrong app pool"
+                    } 
+                }
+                Test-TargetResource @testParams | Should Be $false
+            }
         }
     }
 }
