b132dc9e1d292f7756412d0dd175c66c9a5deb49
Camilo Borges <Camilo.Borges@obs.com.au>
2015-11-22 09:35:23 +0000

    updating files with same changes as https://github.com/BrianFarnhill/xSharePoint/commit/b63c94fa43ba44924aff4dee749feab37c93c3e5

--- Tests/xSharePoint/xSharePoint.Global.Tests.ps1
+++ Tests/xSharePoint/xSharePoint.Global.Tests.ps1
@@ -2,7 +2,6 @@
 param(
     [string] $SharePointCmdletModule = (Join-Path $PSScriptRoot "..\Stubs\SharePoint\15.0.4693.1000\Microsoft.SharePoint.PowerShell.psm1" -Resolve)
 )
-
 $ErrorActionPreference = 'stop'
 Set-StrictMode -Version latest
 
@@ -22,11 +21,13 @@ Describe 'xSharePoint whole of module tests' {
             $mofFiles | ForEach-Object {
                 $fileHasInstallAccount = $false
 
-                $mofSchema = Get-MofSchemaObject $_.FullName
-                $installAccount = $mofSchema.Attributes | Where-Object { $_.Name -eq "InstallAccount" }
-                if (($null -ne $installAccount) -and ($installAccount.State -eq "Required")) {
-                    $mofFilesWithNoInstallAccount += 1
-                    Write-Warning "File $($_.FullName) has InstallAccount listed as a required parameter. After v0.6 of xSharePoint this should be changed to 'write' instead of 'required'"
+                $mofSchemas = Get-MofSchemaObject $_.FullName
+                foreach($mofSchema in $mofSchemas) {
+                    $installAccount = $mofSchema.Attributes | Where-Object { $_.Name -eq "InstallAccount" }
+                    if (($null -ne $installAccount) -and ($installAccount.State -eq "Required")) {
+                        $mofFilesWithNoInstallAccount += 1
+                        Write-Warning "File $($_.FullName) has InstallAccount listed as a required parameter. After v0.6 of xSharePoint this should be changed to 'write' instead of 'required'"
+                    }
                 }
             }
             $mofFilesWithNoInstallAccount | Should Be 0
