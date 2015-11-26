7493e45c664f52b716069a9ca5fd13f3f5ca060b
Brian Farnhill <brian@brianfarnhill.com>
2015-11-26 13:06:48 +1100

    Fixed documentation generation

--- Tests/Generate-xSharePointHelpFiles.ps1
+++ Tests/Generate-xSharePointHelpFiles.ps1
@@ -8,28 +8,31 @@ Import-Module (Join-Path $PSScriptRoot "xSharePoint\xSharePoint.TestHelpers.psm1
 $repoDir = Join-Path $PSScriptRoot "..\" -Resolve
 
 Get-ChildItem "$repoDir\modules\xSharePoint\**\*.schema.mof" -Recurse | `
-    ForEach-Object { 
-        $result = (Get-MofSchemaObject $_.FullName) | Where-Object { $_.ClassName -eq $_.Name.Replace(".schema.mof", "") }
-        Write-Output "Generating help document for $($result.FriendlyName)"
+    ForEach-Object {
+        $mofFileObject = $_ 
+        $result = (Get-MofSchemaObject $_.FullName) | Where-Object { $_.ClassName -eq $mofFileObject.Name.Replace(".schema.mof", "") -and $result.FriendlyName -ne $null }
+        if ($result -ne $null) {
+            Write-Output "Generating help document for $($result.FriendlyName)"
         
-        $output = "NAME" + [Environment]::NewLine
-        $output += "    $($result.FriendlyName)" + [Environment]::NewLine + [Environment]::NewLine
-        $output += "PARAMETERS" + [Environment]::NewLine
+            $output = "NAME" + [Environment]::NewLine
+            $output += "    $($result.FriendlyName)" + [Environment]::NewLine + [Environment]::NewLine
+            $output += "PARAMETERS" + [Environment]::NewLine
 
-        foreach($property in $result.Attributes) {
-            $output += "    $($property.Name) ($($property.State), $($property.DataType)"
-            if ([string]::IsNullOrEmpty($property.ValueMap) -ne $true) {
-                $output += ", Allowed values: "
-                $property.ValueMap | ForEach-Object {
-                    $output += $_ + ", "
+            foreach($property in $result.Attributes) {
+                $output += "    $($property.Name) ($($property.State), $($property.DataType)"
+                if ([string]::IsNullOrEmpty($property.ValueMap) -ne $true) {
+                    $output += ", Allowed values: "
+                    $property.ValueMap | ForEach-Object {
+                        $output += $_ + ", "
+                    }
+                    $output = $output.TrimEnd(" ")
+                    $output = $output.TrimEnd(",")
                 }
-                $output = $output.TrimEnd(" ")
-                $output = $output.TrimEnd(",")
+                $output += ")" + [Environment]::NewLine
             }
-            $output += ")" + [Environment]::NewLine
+
+            $output += [Environment]::NewLine + $result.Documentation.Replace("**Description**", "DESCRIPTION").Replace("**Example**","EXAMPLE")
+
+            $output | Out-File -FilePath (Join-Path $OutPutPath "about_$($result.FriendlyName).help.txt") -Encoding utf8 -Force
         }
-
-        $output += [Environment]::NewLine + $result.Documentation.Replace("**Description**", "DESCRIPTION").Replace("**Example**","EXAMPLE")
-
-        $output | Out-File -FilePath (Join-Path $OutPutPath "about_$($result.FriendlyName).help.txt") -Encoding utf8 -Force
     }
