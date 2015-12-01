b58f65c5f5a0af4bf749e1d3dc419ff0863ca08d
Brian Farnhill <brian@brianfarnhill.com>
2015-12-02 00:27:29 +1100

    Updated search topology to be farm wide resource

---
+++ Modules/xSharePoint/DSCResources/MSFT_xSPSearchTopology/MSFT_xSPSearchTopology.psm1
@@ -0,0 +1,198 @@
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String]   $ServiceAppName,
+        [parameter(Mandatory = $true)]  [System.String[]] $Admin,
+        [parameter(Mandatory = $true)]  [System.String[]] $Crawler,
+        [parameter(Mandatory = $true)]  [System.String[]] $ContentProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $AnalyticsProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $QueryProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $IndexPartition,
+        [parameter(Mandatory = $true)]  [System.String]   $FirstPartitionDirectory,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+
+    $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+        $params = $args[0]
+        $ConfirmPreference = 'None'
+
+        $ssa = Get-SPEnterpriseSearchServiceApplication -Identity $params.ServiceAppName      
+        $currentTopology = $ssa.ActiveTopology
+        
+        $AdminComponents = (Get-SPEnterpriseSearchComponent -SearchTopology $currentTopology | Where-Object { ($_.GetType().Name -eq "AdminComponent") }).ServerName
+        $CrawlComponents = (Get-SPEnterpriseSearchComponent -SearchTopology $currentTopology | Where-Object { ($_.GetType().Name -eq "CrawlComponent") }).ServerName
+        $ContentProcessingComponents = (Get-SPEnterpriseSearchComponent -SearchTopology $currentTopology | Where-Object { ($_.GetType().Name -eq "ContentProcessingComponent") }).ServerName
+        $AnalyticsProcessingComponents = (Get-SPEnterpriseSearchComponent -SearchTopology $currentTopology | Where-Object { ($_.GetType().Name -eq "AnalyticsProcessingComponent") }).ServerName
+        $QueryProcessingComponents = (Get-SPEnterpriseSearchComponent -SearchTopology $currentTopology | Where-Object { ($_.GetType().Name -eq "QueryProcessingComponent") }).ServerName
+        $IndexComponents = (Get-SPEnterpriseSearchComponent -SearchTopology $currentTopology | Where-Object { ($_.GetType().Name -eq "IndexComponent") -and ($_.IndexPartitionOrdinal -eq 0) }).ServerName
+        
+        return @{
+            ServiceAppName = $params.ServiceAppName
+            Admin = $AdminComponents
+            Crawler = $CrawlComponents
+            ContentProcessing = $ContentProcessingComponents
+            AnalyticsProcessing = $AnalyticsProcessingComponents
+            QueryProcessing = $QueryProcessingComponents
+            InstallAccount = $params.InstallAccount
+            FirstPartitionDirectory = $params.FirstPartitionDirectory
+            IndexPartition = $IndexComponents
+        }
+    }
+    return $result
+}
+
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String]   $ServiceAppName,
+        [parameter(Mandatory = $true)]  [System.String[]] $Admin,
+        [parameter(Mandatory = $true)]  [System.String[]] $Crawler,
+        [parameter(Mandatory = $true)]  [System.String[]] $ContentProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $AnalyticsProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $QueryProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $IndexPartition,
+        [parameter(Mandatory = $true)]  [System.String]   $FirstPartitionDirectory,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+
+    Invoke-xSharePointCommand -Credential $InstallAccount -Arguments @($PSBoundParameters, $CurrentValues) -ScriptBlock {
+        $params = $args[0]
+        $CurrentValues = $args[1]
+        $ConfirmPreference = 'None'
+
+
+        $AllSearchServers = @()
+        $AllSearchServers += ($params.Admin | Where-Object { $AllSearchServers.Contains($_) -eq $false })
+        $AllSearchServers += ($params.Crawler | Where-Object { $AllSearchServers.Contains($_) -eq $false })
+        $AllSearchServers += ($params.ContentProcessing | Where-Object { $AllSearchServers.Contains($_) -eq $false })
+        $AllSearchServers += ($params.AnalyticsProcessing | Where-Object { $AllSearchServers.Contains($_) -eq $false })
+        $AllSearchServers += ($params.QueryProcessing | Where-Object { $AllSearchServers.Contains($_) -eq $false })
+        $AllSearchServers += ($params.IndexPartition | Where-Object { $AllSearchServers.Contains($_) -eq $false })
+
+        # Ensure the search service instance is running on all servers
+        foreach($searchServer in $AllSearchServers) {
+            $searchService = Get-SPEnterpriseSearchServiceInstance -Identity $server
+            if($searchService.Status -eq "Offline") {
+                Write-Verbose "Start Search Service Instance"
+                Start-SPEnterpriseSearchServiceInstance -Identity $indexSsi
+            }
+
+            #Wait for Search Service Instance to come online
+            $loopCount = 0
+            $online = Get-SPEnterpriseSearchServiceInstance -Identity $searchService; 
+            do {
+                $online = Get-SPEnterpriseSearchServiceInstance -Identity $searchService; 
+                Write-Verbose "Waiting for service: $($online.TypeName)"
+                $loopCount++
+                Start-Sleep -Seconds 30
+            } 
+            until ($online.Status -eq "Online" -or $loopCount -eq 20)
+        }
+
+        # Create the index partition directory on each remote server
+        foreach($IndexPartitionServer in $params.IndexPartition) {
+            $networkPath = "\\$IndexPartitionServer\" + $params.FirstPartitionDirectory.Replace(":\", "$\")
+            New-Item $networkPath -ItemType Directory -Force
+        }
+        
+        # Get all service service instances to assign topology components to
+        $AllSearchServiceInstances = @{}
+        foreach ($server in $AllSearchServers) {
+            $AllSearchServiceInstances.Add($server, (Get-SPEnterpriseSearchServiceInstance -Identity $server))
+        }
+
+        # Get current topology and prepare a new one
+        $ssa = Get-SPEnterpriseSearchServiceApplication -Identity $params.ServiceAppName
+        $currentTopology = $ssa.ActiveTopology
+        $newTopology = New-SPEnterpriseSearchTopology -SearchApplication $ssa -Clone -SearchTopology $currentTopology
+
+
+        Import-Module (Join-Path $ScriptRoot "..\..\Modules\xSharePoint.SearchTopology\xSharePoint.SearchTopology.psm1" -Resolve)
+
+        # Admin components
+        Set-xSharePointSearchTopologyComponents -ComponentType "AdminComponent" `
+                                                -CurrentServers $CurrentValues.Admin `
+                                                -DesiredServers $params.Admin `
+                                                -NewTopology $newTopology `
+                                                -ServiceInstances $AllSearchServiceInstances
+        # Crawler components
+        Set-xSharePointSearchTopologyComponents -ComponentType "CrawlComponent" `
+                                                -CurrentServers $CurrentValues.Crawler `
+                                                -DesiredServers $params.Crawler `
+                                                -NewTopology $newTopology `
+                                                -ServiceInstances $AllSearchServiceInstances
+        # Content Processing components
+        Set-xSharePointSearchTopologyComponents -ComponentType "ContentProcessingComponent" `
+                                                -CurrentServers $CurrentValues.ContentProcessing `
+                                                -DesiredServers $params.ContentProcessing `
+                                                -NewTopology $newTopology `
+                                                -ServiceInstances $AllSearchServiceInstances
+        # Analytics components
+        Set-xSharePointSearchTopologyComponents -ComponentType "AnalyticsProcessingComponent" `
+                                                -CurrentServers $CurrentValues.AnalyticsProcessing `
+                                                -DesiredServers $params.AnalyticsProcessing `
+                                                -NewTopology $newTopology `
+                                                -ServiceInstances $AllSearchServiceInstances
+        # Query components
+        Set-xSharePointSearchTopologyComponents -ComponentType "QueryProcessingComponent" `
+                                                -CurrentServers $CurrentValues.QueryProcessing `
+                                                -DesiredServers $params.QueryProcessing `
+                                                -NewTopology $newTopology `
+                                                -ServiceInstances $AllSearchServiceInstances
+        # Index components
+        $IndexParams = @{ PartitionId = 0 }
+        if ($params.ContainsKey("FirstPartitionDirectory") -eq $true) { 
+            $IndexParams.Add("PartitionDirectory", $params.FirstPartitionDirectory)
+        } 
+        Set-xSharePointSearchTopologyComponents -ComponentType "IndexComponent" `
+                                                -CurrentServers $CurrentValues.IndexPartition `
+                                                -DesiredServers $params.IndexPartition `
+                                                -NewTopology $newTopology `
+                                                -ServiceInstances $AllSearchServiceInstances `
+                                                @IndexParams
+
+        # Apply the new topology
+        Set-SPEnterpriseSearchTopology -Identity $newTopology
+    }
+}
+
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String]   $ServiceAppName,
+        [parameter(Mandatory = $true)]  [System.String[]] $Admin,
+        [parameter(Mandatory = $true)]  [System.String[]] $Crawler,
+        [parameter(Mandatory = $true)]  [System.String[]] $ContentProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $AnalyticsProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $QueryProcessing,
+        [parameter(Mandatory = $true)]  [System.String[]] $IndexPartition,
+        [parameter(Mandatory = $true)]  [System.String]   $FirstPartitionDirectory,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+    return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues `
+                                              -DesiredValues $PSBoundParameters `
+                                              -ValuesToCheck @(
+                                                  "Admin", 
+                                                  "Crawler", 
+                                                  "ContentProcessing", 
+                                                  "AnalyticsProcessing", 
+                                                  "QueryProcessing",
+                                                  "IndexPartition"
+                                              )
+}
+
+Export-ModuleMember -Function *-TargetResource
---
+++ Modules/xSharePoint/Modules/xSharePoint.SearchTopology/xSharePoint.SearchTopology.psm1
@@ -0,0 +1,68 @@
+function Set-xSharePointSearchTopologyComponents {
+    [CmdletBinding()]
+    param(
+        [parameter(Mandatory = $true)]  [string]    $ComponentType,
+        [parameter(Mandatory = $true)]  [object[]]  $CurrentServers,
+        [parameter(Mandatory = $true)]  [object[]]  $DesiredServers,
+        [parameter(Mandatory = $true)]  [object]    $NewTopology,
+        [parameter(Mandatory = $true)]  [Hashtable] $ServiceInstances,
+        [parameter(Mandatory = $false)] [string]    $PartitionDirectory,
+        [parameter(Mandatory = $false)] [Int]       $PartitionId = 0
+    )
+
+    if ($null -ne $CurrentServers) {
+        $ComponentsToAdd = $DesiredServers
+    } else {
+        $ComponentsToAdd = @()
+        $ComponentsToRemove = @()
+        foreach($Component in ($DesiredServers | Where-Object { $CurrentServers.Contains($_) -eq $false })) {
+            $ComponentsToAdd += $Component
+        }
+        foreach($Component in ($CurrentServers | Where-Object { $DesiredServers.Contains($_) -eq $false })) {
+            $ComponentsToRemove += $Component
+        }
+    }
+    foreach($ComponentToAdd in $ComponentsToAdd) {
+        $NewComponentParams = @{
+            SearchTopology = $NewTopology
+            SearchServiceInstance = $ServiceInstances.$ComponentToAdd
+        }
+        switch($ComponentType) {
+            "AdminComponent" {
+                New-SPEnterpriseSearchAdminComponent @NewComponentParams
+            }
+            "CrawlComponent" {
+                New-SPEnterpriseSearchCrawlComponent @NewComponentParams
+            }
+            "ContentProcessingComponent" {
+                New-SPEnterpriseSearchContentProcessingComponent @NewComponentParams
+            }
+            "AnalyticsProcessingComponent" {
+                New-SPEnterpriseSearchAnalyticsProcessingComponent @NewComponentParams
+            }
+            "QueryProcessingComponent" {
+                New-SPEnterpriseSearchQueryProcessingComponent @NewComponentParams
+            }
+            "IndexComponent" {
+                $NewComponentParams.Add("IndexPartition", $PartitionId)
+                if ($PSBoundParameters.ContainsKey("PartitionDirectory") -eq $true) {
+                    if ([string]::IsNullOrEmpty($PSBoundParameters.PartitionDirectory) -eq $false) {
+                        $NewComponentParams.Add("FirstPartitionDirectory", $PartitionDirectory)
+                    }
+                }
+                New-SPEnterpriseSearchIndexComponent @NewComponentParams
+            }
+        }
+    }
+    foreach($ComponentToRemove in $ComponentsToRemove) {
+        if ($ComponentType -eq "IndexPartition") {
+            $component = Get-SPEnterpriseSearchComponent -SearchTopology $NewTopology | Where-Object {($_.GetType().Name -eq $ComponentType) -and ($_.ServerName -eq $ComponentToRemove) -and ($_.IndexPartitionOrdinal -eq $PartitionId)}
+        } else {
+            $component = Get-SPEnterpriseSearchComponent -SearchTopology $NewTopology | Where-Object {($_.GetType().Name -eq $ComponentType) -and ($_.ServerName -eq $ComponentToRemove)}
+        }
+        if ($null -ne $component) {
+            Remove-SPEnterpriseSearchComponent -Identity $component.ComponentId -SearchTopology $newTopology -confirm:$false
+        }
+        
+    }
+}
