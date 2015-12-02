6ca4052ea6aca488849060c2515c7b9d876da366
Brian Farnhill <brian@brianfarnhill.com>
2015-12-02 20:06:42 +1100

    Updated search index partition to new model for topology components

--- Modules/xSharePoint/DSCResources/MSFT_xSPSearchIndexPartition/MSFT_xSPSearchIndexPartition.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPSearchIndexPartition/MSFT_xSPSearchIndexPartition.psm1
@@ -4,42 +4,28 @@ function Get-TargetResource
     [OutputType([System.Collections.Hashtable])]
     param
     (
-        [parameter(Mandatory = $true)]  [System.UInt32]  $Index,
-        [parameter(Mandatory = $true)]  [System.String]  $Servers,
-        [parameter(Mandatory = $true)]  [System.String]  $RootDirectory,
-        [parameter(Mandatory = $true)]  [System.String]  $ServiceAppName,
-        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
-        [parameter(Mandatory = $true)]  [ValidateSet("Present","Absent")] [System.String] $Ensure
+        [parameter(Mandatory = $true)]  [System.UInt32]    $Index,
+        [parameter(Mandatory = $true)]  [System.String[]]  $Servers,
+        [parameter(Mandatory = $false)] [System.String]    $RootDirectory,
+        [parameter(Mandatory = $true)]  [System.String]    $ServiceAppName,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
     )
 
     $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
         $params = $args[0]
         $ConfirmPreference = 'None'
 
-        $ssa = Get-SPEnterpriseSearchServiceApplication -Identity $params.ServiceAppName 
-        $indexComps = Get-SPEnterpriseSearchComponent -SearchTopology $ssa.ActiveTopology `
-            | Where-Object {($_.GetType().Name -eq "IndexComponent") `
-                -and ($_.IndexPartitionOrdinal -eq $params.Index)}
-
-        if (($indexComps -eq $null) -or ($indexComps.Count -eq 0)) { return @{
-            Index = -1
-            Servers = $params.Servers
-            RootDirectory = $null
-            InstallAccount = $params.InstallAccount
-            Ensure = "Absent"
-        } }
+        $ssa = Get-SPEnterpriseSearchServiceApplication -Identity $params.ServiceAppName      
+        $currentTopology = $ssa.ActiveTopology
         
-        $servers = ""
-        foreach ($indexComp in $indexComps) {
-            $servers += $indexComp.ServerName + ","
-        }
+        $IndexComponents = (Get-SPEnterpriseSearchComponent -SearchTopology $currentTopology | Where-Object { ($_.GetType().Name -eq "IndexComponent") -and ($_.IndexPartitionOrdinal -eq $params.Index) }).ServerName
         
         return @{
             Index = $params.Index
-            Servers = $servers.TrimEnd(",")
+            Servers = $IndexComponents
             RootDirectory = $params.RootDirectory
+            ServiceAPpName = $params.ServiceAppName
             InstallAccount = $params.InstallAccount
-            Ensure = "Present"
         }
     }
     return $result
@@ -50,56 +36,76 @@ function Set-TargetResource
     [CmdletBinding()]
     param
     (
-        [parameter(Mandatory = $true)]  [System.UInt32]  $Index,
-        [parameter(Mandatory = $true)]  [System.String]  $Servers,
-        [parameter(Mandatory = $true)]  [System.String]  $RootDirectory,
-        [parameter(Mandatory = $true)]  [System.String]  $ServiceAppName,
-        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
-        [parameter(Mandatory = $true)]  [ValidateSet("Present","Absent")] [System.String] $Ensure
+        [parameter(Mandatory = $true)]  [System.UInt32]    $Index,
+        [parameter(Mandatory = $true)]  [System.String[]]  $Servers,
+        [parameter(Mandatory = $false)] [System.String]    $RootDirectory,
+        [parameter(Mandatory = $true)]  [System.String]    $ServiceAppName,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
     )
 
-    $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+
+    Invoke-xSharePointCommand -Credential $InstallAccount -Arguments @($PSBoundParameters, $CurrentValues) -ScriptBlock {
         $params = $args[0]
+        $CurrentValues = $args[1]
         $ConfirmPreference = 'None'
 
-        $ssa = Get-SPEnterpriseSearchServiceApplication -Identity $params.ServiceAppName 
-        $newTopology = New-SPEnterpriseSearchTopology -SearchApplication $ssa -Clone -SearchTopology $ssa.ActiveTopology
 
-        $servers = $params["Servers"].Replace(" ", "").Split(",", [StringSplitOptions]::RemoveEmptyEntries)
+        $AllSearchServers = $params.Servers
 
-        foreach($server in $servers) {
-            $ssi = Get-SPEnterpriseSearchServiceInstance -Identity $server
-
-            if($ssi.Status -eq "Offline") {
-                Write-Verbose "Start Search Service Instance on $server"
-                Start-SPEnterpriseSearchServiceInstance -Identity $ssi
+        # Ensure the search service instance is running on all servers
+        foreach($searchServer in $AllSearchServers) {
+            $searchService = Get-SPEnterpriseSearchServiceInstance -Identity $server
+            if($searchService.Status -eq "Offline") {
+                Write-Verbose "Start Search Service Instance"
+                Start-SPEnterpriseSearchServiceInstance -Identity $indexSsi
             }
 
             #Wait for Search Service Instance to come online
             $loopCount = 0
-            $online = Get-SPEnterpriseSearchServiceInstance -Identity $ssi; 
+            $online = Get-SPEnterpriseSearchServiceInstance -Identity $searchService; 
             do {
-                $online = Get-SPEnterpriseSearchServiceInstance -Identity $ssi; 
+                $online = Get-SPEnterpriseSearchServiceInstance -Identity $searchService; 
                 Write-Verbose "Waiting for service: $($online.TypeName)"
                 $loopCount++
                 Start-Sleep -Seconds 30
             } 
             until ($online.Status -eq "Online" -or $loopCount -eq 20)
-
-            if ($params.Ensure -eq "Present") {
-                Write-Verbose "Creating $($params.RootDirectory) on $server"
-                $networkPath = "\\$server\" + $params.RootDirectory.Replace(":\", "$\")
-                New-Item $networkPath -ItemType Directory -Force
-                New-SPEnterpriseSearchIndexComponent -SearchTopology $newTopology -SearchServiceInstance $ssi -IndexPartition $params.Index -RootDirectory $params.RootDirectory
-            }
-            else {
-                $IndexComponent1 = Get-SPEnterpriseSearchComponent -SearchTopology $newTopology | ? {($_.GetType().Name -eq "IndexComponent") -and ($_.ServerName -eq $($ssi.Server.Address)) -and ($_.IndexPartitionOrdinal -eq $params.Index)}
-                if($IndexComponent1) {
-                    $IndexComponent1 | Remove-SPEnterpriseSearchComponent -SearchTopology $newTopology -confirm:$false
-                }
-            }
         }
-        Set-SPEnterpriseSearchTopology -Identity $newTopology        
+
+        # Create the index partition directory on each remote server
+        foreach($IndexPartitionServer in $params.Servers) {
+            $networkPath = "\\$IndexPartitionServer\" + $params.RootDirectory.Replace(":\", "$\")
+            New-Item $networkPath -ItemType Directory -Force
+        }
+        
+        # Get all service service instances to assign topology components to
+        $AllSearchServiceInstances = @{}
+        foreach ($server in $AllSearchServers) {
+            $AllSearchServiceInstances.Add($server, (Get-SPEnterpriseSearchServiceInstance -Identity $server))
+        }
+
+        # Get current topology and prepare a new one
+        $ssa = Get-SPEnterpriseSearchServiceApplication -Identity $params.ServiceAppName
+        $currentTopology = $ssa.ActiveTopology
+        $newTopology = New-SPEnterpriseSearchTopology -SearchApplication $ssa -Clone -SearchTopology $currentTopology
+
+        Import-Module (Join-Path $ScriptRoot "..\..\Modules\xSharePoint.SearchTopology\xSharePoint.SearchTopology.psm1" -Resolve)
+
+        # Index components
+        $IndexParams = @{ PartitionId = $params.Index }
+        if ($params.ContainsKey("RootDirectory") -eq $true) { 
+            $IndexParams.Add("PartitionDirectory", $params.RootDirectory)
+        } 
+        Set-xSharePointSearchTopologyComponents -ComponentType "IndexComponent" `
+                                                -CurrentServers $CurrentValues.Servers `
+                                                -DesiredServers $params.Servers `
+                                                -NewTopology $newTopology `
+                                                -ServiceInstances $AllSearchServiceInstances `
+                                                @IndexParams
+
+        # Apply the new topology
+        Set-SPEnterpriseSearchTopology -Identity $newTopology
     }
 }
 
@@ -109,15 +115,16 @@ function Test-TargetResource
     [OutputType([System.Boolean])]
     param
     (
-        [parameter(Mandatory = $true)]  [System.UInt32]  $Index,
-        [parameter(Mandatory = $true)]  [System.String]  $Servers,
-        [parameter(Mandatory = $true)]  [System.String]  $RootDirectory,
-        [parameter(Mandatory = $true)]  [System.String]  $ServiceAppName,
-        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
-        [parameter(Mandatory = $true)]  [ValidateSet("Present","Absent")] [System.String] $Ensure
+        [parameter(Mandatory = $true)]  [System.UInt32]    $Index,
+        [parameter(Mandatory = $true)]  [System.String[]]  $Servers,
+        [parameter(Mandatory = $false)] [System.String]    $RootDirectory,
+        [parameter(Mandatory = $true)]  [System.String]    $ServiceAppName,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
     )
     $CurrentValues = Get-TargetResource @PSBoundParameters
-    return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues -DesiredValues $PSBoundParameters -ValuesToCheck @("Ensure", "Servers")
+    return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues `
+                                              -DesiredValues $PSBoundParameters `
+                                              -ValuesToCheck @($Servers)
 }
 
 Export-ModuleMember -Function *-TargetResource
