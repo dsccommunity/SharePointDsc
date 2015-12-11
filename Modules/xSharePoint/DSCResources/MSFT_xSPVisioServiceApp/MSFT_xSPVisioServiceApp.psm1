c593bfd6316423202ebac4ed982bfc418b4bbd0d
Brian Farnhill <brian@brianfarnhill.com>
2015-12-11 15:04:29 +1100

    Fixed minor issue in module

--- Modules/xSharePoint/DSCResources/MSFT_xSPVisioServiceApp/MSFT_xSPVisioServiceApp.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPVisioServiceApp/MSFT_xSPVisioServiceApp.psm1
@@ -64,7 +64,7 @@ function Set-TargetResource
 
                 $appPool = Get-SPServiceApplicationPool -Identity $params.ApplicationPool
 
-                Get-SPServiceApplication -Name $params.Name `n
+                Get-SPServiceApplication -Name $params.Name `
                     | Where-Object { $_.TypeName -eq "Visio Graphics Service Application" } `
                     | Set-SPVisioServiceApplication -ServiceApplicationPool $appPool
             }
