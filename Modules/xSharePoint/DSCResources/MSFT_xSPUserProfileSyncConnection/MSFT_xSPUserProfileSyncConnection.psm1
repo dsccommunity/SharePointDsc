2793a3fb566fba4f8fb2b2a6f9373b62762b5633
camiloborges <camilob@gmail.com>
2015-12-12 17:59:19 +1100

    checking in partially completed xSPUserProfileConnection. Tests to be finished in azure environment

---
+++ Modules/xSharePoint/DSCResources/MSFT_xSPUserProfileSyncConnection/MSFT_xSPUserProfileSyncConnection.psm1
@@ -0,0 +1,218 @@
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $Name,
+        [parameter(Mandatory = $true)]  [System.String] $Forest,
+        [parameter(Mandatory = $true)]  [System.Management.Automation.PSCredential] $Credentials,
+        [parameter(Mandatory = $true)] [System.String] $UserProfileService,
+        [parameter(Mandatory = $true)] [System.String] $IncludedOUs,
+        [parameter(Mandatory = $false)] [System.String] $ExcludedOUs,
+        [parameter(Mandatory = $false)] [System.String] $Server,
+        [parameter(Mandatory = $false)] [System.Boolean] $UseSSL,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+
+    Write-Verbose -Message "Getting user profile service sync connection $ConnectionDomain"
+
+    $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+        $params = $args[0]
+        
+
+        $ups = Get-SPServiceApplication -Name $params.UserProfileService -ErrorAction SilentlyContinue 
+        if ($null -eq $serviceApps) { 
+            return $null 
+        }
+       
+
+        If ($null -eq $ups)
+        {
+            return $null
+        }
+        else
+        {
+
+            ##todo: refactor
+            #$context = [Microsoft.Office.Server.ServerContext]::Default
+            $context = [Microsoft.SharePoint.SPServiceContext]::GetContext($ups.ServiceApplicationProxyGroup,[Microsoft.SharePoint.SPSiteSubscriptionIdentifier]::Default)
+            $upcm = New-Object Microsoft.Office.Server.UserProfiles.UserProfileConfigManager($context)
+            $connection = $upcm.ConnectionManager | Where-Object { $_.DisplayName -eq $params.Name}
+            $namingContext = $connection.NamingContexts | select -first 1
+            return @{
+                        UserProfileService = $UserProfileService
+                        Forest = $connection.Server #"contoso.com" #TODO: GetCorrect Forest
+                        Domain = $namingContext.DisplayName
+                        Credentials = "$($connection.AccountDomain)\$($connection.AccountUsername)"
+                        IncludedOUs = $namingContext.ContainersIncluded
+                        ExcludedOUs = $namingContext.ContainersExcluded
+                        Server =$namingContext.PreferredDomainControllers;
+                        UseSSL = $connection.UseSSL;
+                        ConnectionType = $connection.Type;
+
+            }
+        }
+    }
+    return $result
+}
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $Name,
+        [parameter(Mandatory = $true)]  [System.String] $Forest,
+        [parameter(Mandatory = $true)]  [System.Management.Automation.PSCredential] $Credentials,
+        [parameter(Mandatory = $true)] [System.String] $UserProfileService,
+        [parameter(Mandatory = $true)] [System.String] $IncludedOUs,
+        [parameter(Mandatory = $false)] [System.String] $ExcludedOUs,
+        [parameter(Mandatory = $false)] [System.String] $Server,
+        [parameter(Mandatory = $false)] [System.Boolean] $UseSSL,
+        [parameter(Mandatory = $false)] [System.Boolean] $Force,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+   )
+
+    Write-Verbose -Message "Creating user profile service application $Name"
+
+
+    $result = Invoke-xSharePointCommand -Credential $FarmAccount -Arguments $PSBoundParameters -ScriptBlock {
+    $params = $args[0]
+        
+
+    if ($params.ContainsKey("InstallAccount")) { $params.Remove("InstallAccount") | Out-Null }
+    $ups = Get-SPServiceApplication -Name $params.Name -ErrorAction SilentlyContinue 
+                
+    if ($null -eq $ups) { 
+        throw "User Profile Service Application $($params.UserProfileService) not found"
+    }
+    ##todo: refactor    
+    ## $context =   [Microsoft.Office.Server.ServerContext]::Default
+    $context = [Microsoft.SharePoint.SPServiceContext]::GetContext($ups.ServiceApplicationProxyGroup,[Microsoft.SharePoint.SPSiteSubscriptionIdentifier]::Default)
+    $upcm = New-Object Microsoft.Office.Server.UserProfiles.UserProfileConfigManager($context)
+
+    if($upcm.IsSynchronizationRunning())
+    {
+        throw "Synchronization is in Progress."
+    }
+        
+    $securePassword =  $securePassword | ConvertTo-SecureString  $params.Credential.GetNetworkCredential().password -AsPlainText -Force
+    $connection = $upcm.ConnectionManager | Where-Object { $_.DisplayName -eq $params.Name}
+    if($connection -ne $null -and $params.Forest -ne  $connection.Server)
+    {
+            $namingContext = $connection.NamingContexts | select -first 1
+            $connection.SetCredentials($params.Credential.UserName, $securePassword);
+            
+            if($params.ContainsKey("IncludedOUs")){
+                $namingContext.IncludedOUs = params.IncludedOUs
+            }
+            if($params.ContainsKey("ExcludedOUs")){
+                $namingContext.ExcludedOUs = params.ExcludedOUs
+            }else{
+                $namingContext.ExcludedOUs = @()
+            }
+            $connection.RefreshSchema();
+            $connection.Update();
+            return;
+        
+    }else{
+        if($params.Forest -ne  $connection.Server){
+            if($params.ContainsKey("Force")){
+                $connection.Delete();
+            }else{
+                throw "connection exists and forest is different. use force  "
+            }
+            
+        }
+        $namingContext =  New-Object Microsoft.Office.Server.UserProfiles.DirectoryServiceNamingContext(
+                                        $params.Name,
+                                        $params.Forest, 
+                                        $true, 
+                                        [guid]::NewGuid(), 
+                                        $params.IncludedOUs, 
+                                        $params.ExcludedOus,
+                                        $params.Server, 
+                                        $false)
+        $newUPSADConnection =  $upcm.ConnectionManager.AddActiveDirectoryConnection( `
+                                        [Microsoft.Office.Server.UserProfiles.ConnectionType]::ActiveDirectory,  `
+                                        $params.Name, `
+                                        $params.Forest, `
+                                        $params.UseSSL, `
+                                        $params.Credential.UserName, `
+                                        $securePassword, `
+                                        $namingContext, `
+                                        $null, $null `
+                                    )
+
+
+        }
+   
+    }
+}
+
+
+
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $Name,
+        [parameter(Mandatory = $true)]  [System.String] $Forest,
+        [parameter(Mandatory = $true)]  [System.Management.Automation.PSCredential] $Credentials,
+        [parameter(Mandatory = $true)] [System.String] $UserProfileService,
+        [parameter(Mandatory = $true)] [System.String] $IncludedOUs,
+        [parameter(Mandatory = $false)] [System.String] $ExcludedOUs,
+        [parameter(Mandatory = $false)] [System.String] $Server,
+        [parameter(Mandatory = $false)] [System.Boolean] $UseSSL,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+    Write-Verbose -Message "Testing for user profile service sync connection $Name"
+    if ($null -eq $CurrentValues) { return $false }
+    ## todo: test included and excluded OUs
+        return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues -DesiredValues $PSBoundParameters -ValuesToCheck @("Name", "Forest", "UserProfileService", "Server", "UseSSL")
+    
+    if($CurrentValues.IncludedOUs.Count -ne $IncludedOus.Count)
+    {
+        return $false
+    }else{
+        $allGood = $true;
+        $CurrentValues.IncludedOUs | for-eachobject {
+                        if(-not $IncludedOUs.Contains($_)){
+                            $allGood=$false;
+                        }
+                    }
+        if($allGood -eq $false)
+        {
+             return $false
+        }
+    }
+
+    if( ($ExcludedOus -eq $null -and $CurrentValues.ExcludedOus.Count -gt 0) -or 
+        ($CurrentValues.ExcludedOUs.Count -ne $ExcludedOUs.Count)  )
+    {
+        return $false
+    }else{
+        $allGood = $true;
+        $CurrentValues.ExcludedOUs | for-eachobject {
+                        if(-not $ExcludedOUs.Contains($_)){
+                            $allGood=$false;
+                        }
+                    }
+        if($allGood -eq $false)
+        {
+             return $false
+        }
+    }
+
+}
+        
+
+
+Export-ModuleMember -Function *-TargetResource
+
