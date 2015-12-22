67350ab8ec0c36ea2f8399dbff8f43f782adb677
Camilo <camilob@gmail.com>
2015-12-22 10:14:52 +0000

    unit tested, integration tsted

--- Modules/xSharePoint/DSCResources/MSFT_xSPWebApplicationAppDomain/MSFT_xSPWebApplicationAppDomain.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPWebApplicationAppDomain/MSFT_xSPWebApplicationAppDomain.psm1
@@ -4,9 +4,13 @@ function Get-TargetResource
     [OutputType([System.Collections.Hashtable])]
     param
     (
-        [parameter(Mandatory = $false)] [System.String] $AppDomain,
-        [parameter(Mandatory = $true)]  [System.String] $Prefix,
-        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+        [parameter(Mandatory = $true)]  [System.String] $AppDomain,
+        [parameter(Mandatory = $true)] [System.String] $Prefix,
+        [parameter(Mandatory = $false)]  [System.String] $WebApplication,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
+        [parameter(Mandatory = $false)] [System.String] $Zone,
+        [parameter(Mandatory = $false)] [System.UInt32] $Port,
+        [parameter(Mandatory = $false)] [System.Boolean] $SSL
 
     )
 
@@ -15,21 +19,19 @@ function Get-TargetResource
     $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
         $params = $args[0]
         
-        $prefix = Get-SPAppSiteSubscriptionName
+        $prefix = Get-SPAppSiteSubscriptionName -ErrorAction SilentlyContinue
         if($params.ContainsKey("WebApplication")){
             $params.Add("Confirm", $false)
-            $requestParams = @() 
-            $requestParams.Add("WebApplication", $params.WebApplication) 
-            if($requestParams.ContainsKey("Zone")) {
-                $requestParams.Add("Zone", $params.Zone) 
-            }
-                $webAppAppDomain =   Get-SPWebApplicationAppDomain @requestParams
+            $ssl = $params | Remove-xSharePointParamValue -name "SSL" 
+            $appDomain = $params | Remove-xSharePointParamValue -name "AppDomain"
+            $port = $params | Remove-xSharePointParamValue -name "Port"
+            $webAppAppDomain =   Get-SPWebApplicationAppDomain $params
             return @{
                 AppDomain = $webAppAppDomain.AppDomain 
                 WebApplication = $webAppAppDomain.WebApplication
-                Zone = $webAppAppDomain.Zone
+                Zone = $webAppAppDomain.UrlZone
                 Port = $webAppAppDomain.Port
-                SSL = $webAppAppDomain.SecureSocketsLayer
+                SSL = $webAppAppDomain.IsSchemeSSL
                 Prefix= $prefix
                 InstallAccount = $params.InstallAccount
             }
@@ -37,6 +39,9 @@ function Get-TargetResource
             return @{
                 AppDomain = Get-SPAppDomain
                 Prefix= $prefix
+                Zone = $null
+                Port = $null
+                SSL = $null
                 InstallAccount = $params.InstallAccount
             }
         }
@@ -53,38 +58,43 @@ function Set-TargetResource
     [CmdletBinding()]
     param
     (
-        [parameter(Mandatory = $true)]  [System.Management.Automation.PSCredential] $Account,
+        [parameter(Mandatory = $true)] [System.String] $AppDomain,
+        [parameter(Mandatory = $true)] [System.String] $Prefix,
+        [parameter(Mandatory = $false)] [System.String] $WebApplication,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
-        [parameter(Mandatory = $false)] [System.UInt32] $EmailNotification,
-        [parameter(Mandatory = $false)] [System.UInt32] $PreExpireDays,
-        [parameter(Mandatory = $false)] [System.String] $Schedule,
-        [parameter(Mandatory = $true)]  [System.String] $AccountName
+        [parameter(Mandatory = $false)] [System.String] $Zone,
+        [parameter(Mandatory = $false)] [System.UInt32] $Port,
+        [parameter(Mandatory = $false)] [System.Boolean] $SSL
     )
-
-  
 
     Write-Verbose -Message "Updating app domain settings "
     Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
         $params = $args[0]
         $prefix = $params | Remove-xSharePointParamValue -name "Prefix"
         if($prefix -ne $null){
-            Set-SPAppSiteSubscriptionName -Name $params.Prefix -Confirm:$false
+            Set-SPAppSiteSubscriptionName -Name $prefix -Confirm:$false
         }
 
         if($params.ContainsKey("WebApplication")){
+            $getParams = @{}
             $params.Add("Confirm", $false)
-            if((Get-SPWebApplicationAppDomain  -WebApplication $params.WebApplication )-ne $null){
-                Update-SPWebApplicationAppDomain @params  
-            }else{
-                New-SPWebApplicationAppDomain @params
+            $getParams.Add("ErrorAction", 0)
+            $getParams.Add("WebApplication", $params.WebApplication)
+            if($params.ContainsKey("Zone")){
+                $getPArams.Add("Zone",$Params.Zone)
             }
+
+            $appDomain = (Get-SPWebApplicationAppDomain  @getParams )
+            $params = $params | Rename-xSharePointParamValue -oldName  "SSL"  -newName "SecureSocketsLayer"
+            if($appDomain -ne $null){
+                Remove-SPWebApplicationAppDomain $appDomain
+                Sleep -Seconds 1
+            }
+               New-SPWebApplicationAppDomain @params
+            
         }else{
             Set-SPAppDomain $params.AppDomain  -Confirm:$false    
         }
-        
-
-
-
     }
 }
 
@@ -95,14 +105,18 @@ function Test-TargetResource
     [OutputType([System.Boolean])]
     param
     (
-        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
         [parameter(Mandatory = $true)] [System.String] $AppDomain,
-        [parameter(Mandatory = $false)]  [System.String] $Prefix
+        [parameter(Mandatory = $true)] [System.String] $Prefix,
+        [parameter(Mandatory = $false)] [System.String] $WebApplication,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
+        [parameter(Mandatory = $false)] [System.String] $Zone,
+        [parameter(Mandatory = $false)] [System.UInt32] $Port,
+        [parameter(Mandatory = $false)] [System.Boolean] $SSL
     )
 
     $CurrentValues = Get-TargetResource @PSBoundParameters
     Write-Verbose -Message "Testing app domain settings"
-    return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues -DesiredValues $PSBoundParameters -ValuesToCheck @("AppDomain", "Prefix") 
+    return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues -DesiredValues $PSBoundParameters -ValuesToCheck @("AppDomain", "Prefix", "Zone", "SSL", "WebApplication") 
 }
 
 
--- Tests/xSharePoint/xSharePoint.xSPWebApplicationAppDomain.Tests.ps1
+++ Tests/xSharePoint/xSharePoint.xSPWebApplicationAppDomain.Tests.ps1
@@ -9,17 +9,41 @@ Set-StrictMode -Version latest
 $RepoRoot = (Resolve-Path $PSScriptRoot\..\..).Path
 $Global:CurrentSharePointStubModule = $SharePointCmdletModule
 
-$ModuleName = "MSFT_xSPWebAppDomain"
+$ModuleName = "MSFT_xSPWebApplicationAppDomain"
 Import-Module (Join-Path $RepoRoot "Modules\xSharePoint\DSCResources\$ModuleName\$ModuleName.psm1")
 
-Describe "xSPWebAppDomain" {
+Describe "xSPWebApplicationAppDomain" {
     InModuleScope $ModuleName {
         $testParams = @{
-            WebAppUrl = "http://sites.sharepoint.com"
-            RelativeUrl = "teams"
-            Explicit = $false
-            HostHeader = $false
+            AppDomain = "contosointranetapps.com"
+            Prefix = "app"
+            WebApplication ="http://portal.contoso.com"
+            Zone = "Default"
+            Port=80;
+            SSL=$false
         }
+        $testParamsBasic = @{
+            AppDomain = "contosointranetapps.com"
+            Prefix = "app"
+        }
+
+        $returnMatch =@{
+                AppDomain = "contosointranetapps.com"
+                WebApplication = "http://portal.contoso.com"
+                UrlZone = "Default"
+                Port = "80"
+                IsSchemeSSL=$false
+                Prefix= "app"
+        } 
+        $returnNotMatch =@{
+                AppDomain = "litwareintranetapps.com"
+                WebApplication = "http://portal.contoso.com"
+                UrlZone = "Default"
+                Port = "80"
+                IsSchemeSSL=$false
+                Prefix= "app"
+        } 
+
         Import-Module (Join-Path ((Resolve-Path $PSScriptRoot\..\..).Path) "Modules\xSharePoint")
         
         Mock Invoke-xSharePointCommand { 
@@ -29,11 +53,11 @@ Describe "xSPWebAppDomain" {
         Import-Module $Global:CurrentSharePointStubModule -WarningAction SilentlyContinue 
         Mock New-SPManagedPath { }
 
-        Context "App Service isn't available" {
-            Mock Get-SPManagedPath { return $null }
-
-            It "returns null from the get method" {
-                Get-TargetResource @testParams | Should BeNullOrEmpty
+        Context "Subscription Service isn't available" {
+            Mock Get-SPAppSiteSubscriptionName {  return $null }
+            
+            It "returns values from the get method" {
+                Get-TargetResource @testParams | Should Not BeNullOrEmpty
             }
 
             It "returns false from the test method" {
@@ -41,20 +65,23 @@ Describe "xSPWebAppDomain" {
             }
 
             It "throws exception when executed" {
-                Set-TargetResource @testParams
-                Assert-MockCalled New-SPManagedPath
+                Mock  Set-SPAppSiteSubscriptionName {throw 'Exception'}
+                {Set-TargetResource @testParams}| Should Throw
+               
+            }
+            It "subscription is available, but app isn't throws exception when executed" {
+                Mock New-SPWebApplicationAppDomain  {throw 'Exception'}
+                Mock Set-SPAppSiteSubscriptionName {}
+                {Set-TargetResource @testParams}| Should Throw
+                Assert-MockCalled Set-SPAppSiteSubscriptionName
             }
 
         }
-
-        Context "App Management Is Available. Settings are saved" {
-            Mock Get-SPManagedPath { return @{
-                Name = $testParams.RelativeUrl
-                Type = "ExplicitInclusion"
-            } }
-
-            It "returns null from the get method" {
-                Get-TargetResource @testParams | Should BeNullOrEmpty
+        Context "App Management Is Available. adds new web app app domain Settings" {
+            Mock Get-SPWebApplicationAppDomain { return $null}
+            Mock New-SPWebApplicationAppDomain  {}
+            It "returns values from the get method" {
+                Get-TargetResource @testParams | Should Not BeNullOrEmpty
             }
 
             It "returns false from the test method" {
@@ -63,9 +90,49 @@ Describe "xSPWebAppDomain" {
 
             It "saves settings when executed" {
                 Set-TargetResource @testParams
-                Assert-MockCalled New-SPManagedPath
+                Assert-MockCalled New-SPWebApplicationAppDomain 
             }
         }
+
+        Context "App Management Is Available. updates web app app domain Settings" {
+            Mock Get-SPWebApplicationAppDomain { return $returnNotMatch
+            
+            }
+            Mock Remove-SPWebApplicationAppDomain{}
+            Mock New-SPWebApplicationAppDomain  {}
+            It "returns values from the get method" {
+                Get-TargetResource @testParams | Should Not BeNullOrEmpty
+            }
+
+            It "returns false from the test method" {
+                Test-TargetResource @testParams | Should Be $false
+            }
+
+            It "saves settings when executed" {
+                Set-TargetResource @testParams
+                Assert-MockCalled Remove-SPWebApplicationAppDomain 
+                Assert-MockCalled New-SPWebApplicationAppDomain 
+            }
+        }
+        Context "Basic: App Management Is Available. updates app domain Settings" {
+            Mock Get-SPAppDomain { return $null            }
+            Mock Set-SPAppDomain{}
+            It "returns values from the get method" {
+                Get-TargetResource @testParamsBasic | Should Not BeNullOrEmpty
+                Assert-MockCalled Get-SPAppDomain
+            }
+
+            It "returns false from the test method" {
+                Test-TargetResource @testParamsBasic | Should Be $false
+            }
+
+            It "saves settings when executed" {
+                Set-TargetResource @testParamsBasic
+                Assert-MockCalled Set-SPAppDomain
+            }
+        }
+
+        
     }    
 }
 
