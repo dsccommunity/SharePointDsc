76540264fa2ee938b53f2b1ea46c227dff3c2c70
Camilo <camilob@gmail.com>
2015-12-23 16:55:02 +0000

    adding untracked files

---
+++ Modules/xSharePoint/DSCResources/MSFT_xSPWordAutomationServiceApp/MSFT_xSPWordAutomationServiceApp.psm1
@@ -0,0 +1,104 @@
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $Name,
+        [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
+        [parameter(Mandatory = $true)]  [System.String] $DatabaseName,
+        [parameter(Mandatory = $false)] [System.String] $DatabaseServer,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $DatabaseCredentials,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+
+        Write-Verbose -Message "Getting Word Automation service app '$Name'"
+
+        $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+        $params = $args[0]
+        
+        $serviceApps = Get-SPServiceApplication -Name $params.Name -ErrorAction SilentlyContinue 
+        if ($null -eq $serviceApps) { 
+            return $null 
+        }
+        $serviceApp = $serviceApps | Where-Object { $_.TypeName -eq "Word Automation Services" }
+
+        If ($null -eq $serviceApp) { 
+            return $null 
+        } else {
+            $returnVal =  @{
+                Name = $serviceApp.DisplayName
+                ApplicationPool = $serviceApp.ApplicationPool.Name
+                DatabaseName = $serviceApp.Databases.Name
+                DatabaseServer = $serviceApp.Databases.Server.Name
+
+            }
+            return $returnVal
+        }
+    }
+    return $result
+}
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $Name,
+        [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
+        [parameter(Mandatory = $true)]  [System.String] $DatabaseName,
+        [parameter(Mandatory = $false)] [System.String] $DatabaseServer,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $DatabaseCredentials,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+
+    $result = Get-TargetResource @PSBoundParameters
+
+    if ($result -eq $null) { 
+        Write-Verbose -Message "Creating Word Automation Service Application $Name"
+        Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+            $params = $args[0]
+            if($params.ContainsKey("InstallAccount")){ $params.Remove("InstallAccount")}
+
+            New-SPWordConversionServiceApplication @params 
+        }
+    }
+    else {
+        if ($ApplicationPool -ne $result.ApplicationPool) {
+            Write-Verbose -Message "Updating Word Automation Service Application $Name"
+            Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+                $params = $args[0]               
+
+                $appPool = Get-SPServiceApplicationPool -Identity $params.ApplicationPool
+
+                Get-SPServiceApplication -Name $params.Name `
+                    | Where-Object { $_.TypeName -eq "Word Automation Services" } `
+                    | Set-SPWordConversionServiceApplication -ApplicationPool $appPool
+            }
+        }
+    }
+}
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $Name,
+        [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
+        [parameter(Mandatory = $true)]  [System.String] $DatabaseName,
+        [parameter(Mandatory = $false)] [System.String] $DatabaseServer,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $DatabaseCredentials,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+    )
+    
+    Write-Verbose -Message "Testing for Word Automation Service Application '$Name'"
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+    
+    if ($null -eq $CurrentValues) { return $false }
+    return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues -DesiredValues $PSBoundParameters -ValuesToCheck @("ApplicationPool")
+}
+
+Export-ModuleMember -Function *-TargetResource
