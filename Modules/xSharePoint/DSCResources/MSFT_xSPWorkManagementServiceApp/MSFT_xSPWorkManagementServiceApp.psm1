fe21ff75399cd8772fb4dd6fd9a5f2b17010341f
Camilo <camilob@gmail.com>
2016-01-29 02:02:17 +0000

    unit tests

--- Modules/xSharePoint/DSCResources/MSFT_xSPWorkManagementServiceApp/MSFT_xSPWorkManagementServiceApp.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPWorkManagementServiceApp/MSFT_xSPWorkManagementServiceApp.psm1
@@ -5,8 +5,8 @@ function Get-TargetResource
     param
     (
         [parameter(Mandatory = $true)]  [System.String] $Name,
-        [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
-        [parameter(Mandatory = $true)]  [ValidateSet("Present","Absent")] [System.String] $Ensure,
+        [parameter(Mandatory = $false)]  [System.String] $ApplicationPool,
+        [parameter(Mandatory = $false)]  [ValidateSet("Present","Absent")] [System.String] $Ensure,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
         [parameter(Mandatory = $false)] [System.UInt32] $MinimumTimeBetweenEwsSyncSubscriptionSearches, 
         [parameter(Mandatory = $false)] [System.UInt32] $MinimumTimeBetweenProviderRefreshes, 
@@ -46,8 +46,8 @@ function Set-TargetResource
     param
     (
         [parameter(Mandatory = $true)]  [System.String] $Name,
-        [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
-        [parameter(Mandatory = $true)]  [ValidateSet("Present","Absent")] [System.String] $Ensure,
+        [parameter(Mandatory = $false)]  [System.String] $ApplicationPool,
+        [parameter(Mandatory = $false)]  [ValidateSet("Present","Absent")] [System.String] $Ensure,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
         [parameter(Mandatory = $false)] [System.UInt32] $MinimumTimeBetweenEwsSyncSubscriptionSearches, 
         [parameter(Mandatory = $false)] [System.UInt32] $MinimumTimeBetweenProviderRefreshes, 
@@ -56,89 +56,66 @@ function Set-TargetResource
         [parameter(Mandatory = $false)] [System.UInt32] $NumberOfUsersEwsSyncWillProcessAtOnce, 
         [parameter(Mandatory = $false)] [System.UInt32] $NumberOfUsersPerEwsSyncBatch 
     )
-
-    $result = Get-TargetResource @PSBoundParameters
-    $setParameters = @(
-                    "ApplicationPool",
-                    "MinimumTimeBetweenEwsSyncSubscriptionSearches",
-                    "MinimumTimeBetweenProviderRefreshes",
-                    "MinimumTimeBetweenSearchQueries",
-                    "Name",
-                    "NumberOfSubscriptionSyncsPerEwsSyncRun",
-                    "NumberOfUsersEwsSyncWillProcessAtOnce",
-                    "NumberOfUsersPerEwsSyncBatch"
-              )
-   $newParamters = @(
-                    "ApplicationPool",
-                    "Name" 
-                    ) 
-    if ($result -eq $null) { 
-        Write-Verbose -Message "Creating work management Service Application $Name"
-        Invoke-xSharePointCommand -Credential $InstallAccount -Arguments ($PSBoundParameters, $setParameters, $newParameters ) -ScriptBlock {
+    if($Ensure -ne "Absent" -and $ApplicationPool -eq $null){
+        throw "Parameter ApplicationPool is required unless service is being removed(Ensure='Absent')"
+    }
+    <#
+    if ($Ensure -eq  "Absent") {
+        if($result -ne $null){
+            Write-Verbose -Message "Removing Work management Service Application $Name"
+            Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters  -ScriptBlock {
             $params = $args[0]
-            $setParams = $params.GetEnumerator() | ? {$args[1] -contains $_.Name} 
-            $newParams = $params.GetEnumerator() | ? {$args[2] -contains $_.Name}
+            }
+        }
+    }#>
+    Write-Verbose -Message "Creating work management Service Application $Name"
+    Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+        $params = $args[0]
+        $appService =  Get-SPServiceApplication -Name $params.Name `
+        | Where-Object { $_.TypeName -eq "Work Management Service Application"  }
+
+        if($appService -ne $null -and $params.ContainsKey("Ensure") -and $params.Ensure -eq "Absent")
+        {
+            #remove existing app
+            
+            Remove-SPServiceApplication $appService 
+            return;
+        } elseif ( $appService -eq $null){
+            $newParams = @{}
+            $newParams.Add("Name", $params.Name) 
+            $newParams.Add("ApplicationPool", $params.ApplicationPool) 
+
             $appService = New-SPWorkManagementServiceApplication @newParams
             New-SPWorkManagementServiceApplicationProxy -Name "$($params.Name) Proxy" -DefaultProxyGroup -ServiceApplication $appService -ea Stop | Out-Null
-            if ($setParams.ContainsKey("MinimumTimeBetweenEwsSyncSubscriptionSearches")) { 
-                $setParams.MinimumTimeBetweenEwsSyncSubscriptionSearches = New-TimeSpan -Days $setParams.MinimumTimeBetweenEwsSyncSubscriptionSearches
-            }
-            if ($setParams.ContainsKey("MinimumTimeBetweenProviderRefreshes")) { 
-                $setParams.MinimumTimeBetweenProviderRefreshes = New-TimeSpan -Days $setParams.MinimumTimeBetweenProviderRefreshes
-            }
-            if ($setParams.ContainsKey("MinimumTimeBetweenSearchQueries")) { 
-                $setParams.MinimumTimeBetweenSearchQueries = New-TimeSpan -Days $setParams.MinimumTimeBetweenSearchQueries
-            }
-            $setParams.Add("Confirm", $false)
-            Set-SPWorkManagementServiceApplicationProxy @setPArams | Out-Null
+            
         }
-         
-        #}
-    }else {
-        if ($Ensure -eq  "Absent") {
-            $appService =  Get-SPServiceApplication -Name $params.Name `
-                | Where-Object { $_.TypeName -eq "Work Management Service Application"  }
-            Remove-SPServiceApplication $appService 
-        }else{
-            Write-Verbose -Message "Updating Work management Service Application $Name"
-            Invoke-xSharePointCommand -Credential $InstallAccount -Arguments ($PSBoundParameters, $setParameters ) -ScriptBlock {
-                $params = $args[0]
-                $setParams = $params.GetEnumerator() | ? {$args[1] -contains $_.Name} 
-                #$appService =  Get-SPServiceApplication -Name $params.Name `
-                #    | Where-Object { $_.TypeName -eq "Work Management Service Application"  }
-                $setParams.Add("Confirm", $false)
-                <#$appPool=$null
-                if($setParams.ContainsKey("ApplicationPool")){
-                    $appPool = Get-SPServiceApplicationPool -Identity $params.ApplicationPool
-                    $setParams.ApplicationPool = $appPool
-                }#>
-                if ($setParams.ContainsKey("MinimumTimeBetweenEwsSyncSubscriptionSearches")) { 
-                    $setParams.MinimumTimeBetweenEwsSyncSubscriptionSearches = New-TimeSpan -Days $setParams.MinimumTimeBetweenEwsSyncSubscriptionSearches
-                }
-                if ($setParams.ContainsKey("MinimumTimeBetweenProviderRefreshes")) { 
-                    $setParams.MinimumTimeBetweenProviderRefreshes = New-TimeSpan -Days $setParams.MinimumTimeBetweenProviderRefreshes
-                }
-                if ($setParams.ContainsKey("MinimumTimeBetweenSearchQueries")) { 
-                    $setParams.MinimumTimeBetweenSearchQueries = New-TimeSpan -Days $setParams.MinimumTimeBetweenSearchQueries
-                }
+        $setParams = @{}
+        if ($params.ContainsKey("MinimumTimeBetweenEwsSyncSubscriptionSearches")) { $setParams.Add("MinimumTimeBetweenEwsSyncSubscriptionSearches", $params.MinimumTimeBetweenEwsSyncSubscriptionSearches) }
+        if ($params.ContainsKey("MinimumTimeBetweenProviderRefreshes")) { $setParams.Add("MinimumTimeBetweenProviderRefreshes", $params.MinimumTimeBetweenProviderRefreshes) }
+        if ($params.ContainsKey("MinimumTimeBetweenSearchQueries")) { $setParams.Add("MinimumTimeBetweenSearchQueries", $params.MinimumTimeBetweenSearchQueries) }
+        if ($params.ContainsKey("NumberOfSubscriptionSyncsPerEwsSyncRun")) { $setParams.Add("NumberOfSubscriptionSyncsPerEwsSyncRun", $params.NumberOfSubscriptionSyncsPerEwsSyncRun) }
+        if ($params.ContainsKey("NumberOfUsersEwsSyncWillProcessAtOnce")) { $setParams.Add("NumberOfUsersEwsSyncWillProcessAtOnce", $params.NumberOfUsersEwsSyncWillProcessAtOnce) }
+        if ($params.ContainsKey("NumberOfUsersPerEwsSyncBatch")) { $setParams.Add("NumberOfUsersPerEwsSyncBatch", $params.NumberOfUsersPerEwsSyncBatch) }
 
-                Set-SPWorkManagementServiceApplication @setPArams | Out-Null
-            }
-            <#
-            Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
-                $params = $args[0]
-                $appPool = Get-SPServiceApplicationPool -Identity $params.ApplicationPool
-                
-                $appService =  Get-SPServiceApplication -Name $params.Name `
-                    | Where-Object { $_.TypeName -eq "Work Management Service Application"  } 
-                $AppService.ApplicationPool = $appPool
-                $AppService.Update()
-            }#>
+        $setParams.Add("Name", $params.Name) 
+        $setParams.Add("ApplicationPool", $params.ApplicationPool) 
+
+        if ($setParams.ContainsKey("MinimumTimeBetweenEwsSyncSubscriptionSearches")) { 
+            $setParams.MinimumTimeBetweenEwsSyncSubscriptionSearches = New-TimeSpan -Days $setParams.MinimumTimeBetweenEwsSyncSubscriptionSearches
         }
+        if ($setParams.ContainsKey("MinimumTimeBetweenProviderRefreshes")) { 
+            $setParams.MinimumTimeBetweenProviderRefreshes = New-TimeSpan -Days $setParams.MinimumTimeBetweenProviderRefreshes
+        }
+        if ($setParams.ContainsKey("MinimumTimeBetweenSearchQueries")) { 
+            $setParams.MinimumTimeBetweenSearchQueries = New-TimeSpan -Days $setParams.MinimumTimeBetweenSearchQueries
+        }
+        $setParams.Add("Confirm", $false)
+        $appService =  Get-SPServiceApplication -Name $params.Name `
+            | Where-Object { $_.TypeName -eq "Work Management Service Application"  }
+          
+        $appService | Set-SPWorkManagementServiceApplication @setPArams | Out-Null
     }
 }
-#}
-    
 
 
 function Test-TargetResource
@@ -148,8 +125,8 @@ function Test-TargetResource
     param
     (
         [parameter(Mandatory = $true)]  [System.String] $Name,
-        [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
-        [parameter(Mandatory = $true)]  [ValidateSet("Present","Absent")] [System.String] $Ensure,
+        [parameter(Mandatory = $false)]  [System.String] $ApplicationPool,
+        [parameter(Mandatory = $false)]  [ValidateSet("Present","Absent")] [System.String] $Ensure,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount,
         [parameter(Mandatory = $false)] [System.UInt32] $MinimumTimeBetweenEwsSyncSubscriptionSearches, 
         [parameter(Mandatory = $false)] [System.UInt32] $MinimumTimeBetweenProviderRefreshes, 
