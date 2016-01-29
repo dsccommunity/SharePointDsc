d6b95662f97ef5e386db464ec8ef6e385ec34f94
Camilo <camilob@gmail.com>
2016-01-29 03:07:46 +0000

    adding admin settings back in. and updating unit tests

--- Tests/xSharePoint/xSharePoint.xSPWorkManagementServiceApp.Tests.ps1
+++ Tests/xSharePoint/xSharePoint.xSPWorkManagementServiceApp.Tests.ps1
@@ -24,9 +24,9 @@ Describe "xSPWorkManagement" {
             MinimumTimeBetweenEwsSyncSubscriptionSearches =10
             MinimumTimeBetweenProviderRefreshes=10
             MinimumTimeBetweenSearchQueries=10
-            NumberOfSubscriptionSyncsPerEwsSyncRun=5
-            NumberOfUsersEwsSyncWillProcessAtOnce=5
-            NumberOfUsersPerEwsSyncBatch=500
+            NumberOfSubscriptionSyncsPerEwsSyncRun=10
+            NumberOfUsersEwsSyncWillProcessAtOnce=10
+            NumberOfUsersPerEwsSyncBatch=10
         }
 
         Import-Module (Join-Path ((Resolve-Path $PSScriptRoot\..\..).Path) "Modules\xSharePoint")
@@ -37,6 +37,29 @@ Describe "xSPWorkManagement" {
         }
         
         Import-Module $Global:CurrentSharePointStubModule -WarningAction SilentlyContinue
+                Context "When a service application exists and Ensure equals 'absent'" {
+            $testParamsAbsent = @{
+                Name = "Test Work Management App"
+                Ensure = "Absent"
+            }
+            Mock Get-SPServiceApplication { 
+                return @(@{
+                    TypeName = "Work Management Service Application"
+                    DisplayName = $testParamsAbsent.Name
+                    ApplicationPool = @{ Name = "Wrong App Pool Name" }
+                })
+            }
+            Mock Remove-SPServiceApplication{ }
+
+            It "returns true when the Test method is called" {
+                Test-TargetResource @testParamsAbsent | Should Be $true
+            }
+
+            It "calls the remove service app cmdlet from the set method" {
+                Set-TargetResource @testParamsAbsent
+                Assert-MockCalled Remove-SPServiceApplication
+            }
+        }
 
         Context "When no service applications exist in the current farm" {
 
@@ -64,13 +87,27 @@ Describe "xSPWorkManagement" {
             Mock Set-SPWorkManagementServiceApplication { }
             Mock New-SPWorkManagementServiceApplication { }
             Mock New-SPWorkManagementServiceApplicationProxy { }
-
-            Mock Get-SPServiceApplication { return @(@{
-                TypeName = "Some other service app type"
-            }) }
-
+            $Global:GetSpServiceApplicationCalled=$false
+            Mock Get-SPServiceApplication { 
+                if($Global:GetSpServiceApplicationCalled -eq $false){
+                    $Global:GetSpServiceApplicationCalled=$true;
+                    return @(@{
+                    TypeName = "Some other service app type"
+                    })
+                }
+                return @(@{
+                    TypeName = "Work Management Service Application" 
+                        })
+            }
+            
+        
             It "returns null from the Get method" {
                 Get-TargetResource @testParams | Should BeNullOrEmpty
+                Assert-MockCalled Get-SPServiceApplication -ParameterFilter { $Name -eq $testParams.Name } 
+            }
+
+            It "creates  new app from the Get method" {
+                Set-TargetResource @testParams | Should BeNullOrEmpty
                 Assert-MockCalled Get-SPServiceApplication -ParameterFilter { $Name -eq $testParams.Name } 
                 Assert-MockCalled Set-SPWorkManagementServiceApplication -ParameterFilter { $Name -eq $testParams.Name } 
             }
@@ -81,18 +118,28 @@ Describe "xSPWorkManagement" {
             Mock Get-SPServiceApplication { 
                 return @(@{
                     TypeName = "Work Management Service Application"
-                    DisplayName = $testParams.Name
-                    ApplicationPool = @{ Name = $testParams.ApplicationPool }
+                    DisplayName = $testParamsComplete.Name
+                    ApplicationPool = @{ Name = $testParamsComplete.ApplicationPool }
+                    AdminSettings = @{
+                            MinimumTimeBetweenEwsSyncSubscriptionSearches =  (new-timespan -minutes 10)
+                            MinimumTimeBetweenProviderRefreshes= (new-timespan -minutes 10)
+                            MinimumTimeBetweenSearchQueries= (new-timespan -minutes 10)
+                            NumberOfSubscriptionSyncsPerEwsSyncRun=10
+                            NumberOfUsersEwsSyncWillProcessAtOnce=  10
+                            NumberOfUsersPerEwsSyncBatch=  10
+            
+                    }
+
                 })
             }
 
             It "returns values from the get method" {
-                Get-TargetResource @testParams | Should Not BeNullOrEmpty
-                Assert-MockCalled Get-SPServiceApplication -ParameterFilter { $Name -eq $testParams.Name } 
+                Get-TargetResource @testParamsComplete | Should Not BeNullOrEmpty
+                Assert-MockCalled Get-SPServiceApplication -ParameterFilter { $Name -eq $testParamsComplete.Name } 
             }
 
             It "returns true when the Test method is called" {
-                Test-TargetResource @testParams | Should Be $true
+                Test-TargetResource @testParamsComplete | Should Be $true
             }
         }
 
@@ -102,6 +149,16 @@ Describe "xSPWorkManagement" {
                     TypeName = "Work Management Service Application"
                     DisplayName = $testParams.Name
                     ApplicationPool = @{ Name = "Wrong App Pool Name" }
+                    AdminSettings = @{
+                            MinimumTimeBetweenEwsSyncSubscriptionSearches =  (new-timespan -minutes 10)
+                            MinimumTimeBetweenProviderRefreshes= (new-timespan -minutes 10)
+                            MinimumTimeBetweenSearchQueries= (new-timespan -minutes 10)
+                            NumberOfSubscriptionSyncsPerEwsSyncRun=10
+                            NumberOfUsersEwsSyncWillProcessAtOnce=  10
+                            NumberOfUsersPerEwsSyncBatch=  10
+            
+                    }
+
                 })
             }
             Mock Set-SPWorkManagementServiceApplication { }
@@ -113,30 +170,7 @@ Describe "xSPWorkManagement" {
             It "calls the update service app cmdlet from the set method" {
                 Set-TargetResource @testParamsComplete
                 Assert-MockCalled Set-SPWorkManagementServiceApplication
-                Assert-MockCalled Get-SPWorkManagementServiceApplication
-            }
-        }
-        Context "When a service application exists and Ensure equals 'absent'" {
-            $testParamsAbsent = @{
-                Name = "Test Work Management App"
-            Ensure = "Absent"
-            }
-            Mock Get-SPServiceApplication { 
-                return @(@{
-                    TypeName = "Work Management Service Application"
-                    DisplayName = $testParamsAbsent.Name
-                    ApplicationPool = @{ Name = "Wrong App Pool Name" }
-                })
-            }
-            Mock Remove-SPServiceApplication{ }
-
-            It "returns false when the Test method is called" {
-                Test-TargetResource $testParamsAbsent | Should Be $false
-            }
-
-            It "calls the update service app cmdlet from the set method" {
-                Set-TargetResource $testParamsAbsent
-                Assert-MockCalled Remove-SPServiceApplication
+                Assert-MockCalled Get-SPServiceApplication
             }
         }
 
