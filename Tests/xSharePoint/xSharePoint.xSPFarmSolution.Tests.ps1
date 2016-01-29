3c3e248dea96e0166a53d17d0e8ee2b20fd68e79
Mike Kaufmann <wulfland@hotmail.com>
2016-01-29 13:08:07 +0100

    Added tests for other use cases.

--- Tests/xSharePoint/xSharePoint.xSPFarmSolution.Tests.ps1
+++ Tests/xSharePoint/xSharePoint.xSPFarmSolution.Tests.ps1
@@ -29,11 +29,13 @@ Describe "xSPFarmSolution" {
             Ensure          = "Present"
             Version         = "1.0.0.0"
             WebApplications = @("http://app1", "http://app2")
+            Verbose         = $true
         }
         
         Import-Module $Global:CurrentSharePointStubModule -WarningAction SilentlyContinue
 
         Context "The solution isn't installed, but should be" {
+
             $global:SolutionAdded = $false
             Mock Get-SPSolution { 
                 if ($global:SolutionAdded) { 
@@ -49,19 +51,13 @@ Describe "xSPFarmSolution" {
                 return $solution
             } -Verifiable
             Mock Install-SPSolution { } -Verifiable
-            Mock WaitFor-SolutionJob { }
+            Mock WaitFor-SolutionJob { } -Verifiable
 
             $getResults = Get-TargetResource @testParams
 
-            It "returns Ensure 'Absent' from the get method" {
+            It "returns the expected empty values from the get method" {
                 $getResults.Ensure | Should Be "Absent"
-            }
-
-            It "returns Version '0.0.0.0' from the get method" {
                 $getResults.Version | Should Be "0.0.0.0"
-            }
-
-            It "returns Deployed 'false' from the get method" {
                 $getResults.Deployed | Should Be $false
             }
 
@@ -69,23 +65,193 @@ Describe "xSPFarmSolution" {
                 Test-TargetResource @testParams | Should Be $false
             }
 
-            It "uploads the solution to the farm" {
+            It "uploads and installes the solution to the farm" {
                 Set-TargetResource @testParams
 
                 Assert-MockCalled Add-SPSolution 
+                Assert-MockCalled Install-SPSolution
+                Assert-MockCalled WaitFor-SolutionJob 
             }
         }
 
         Context "The solution is installed, but should not be"{
+
+            $testParams.Ensure = "Absent"
+
+            Mock Get-SPSolution {
+                return [pscustomobject]@{
+                    Deployed                = $true
+                    Properties              = @{ Version = "1.0.0.0" }
+                    DeployedWebApplications = @( [pscustomobject]@{Url="http://app1"}, [pscustomobject]@{Url="http://app2"})
+                    ContainsGlobalAssembly  = $true
+                }
+            } -Verifiable
+
+            Mock Uninstall-SPSolution { } -Verifiable
+            Mock WaitFor-SolutionJob { } -Verifiable
+            Mock Remove-SPSolution { } -Verifiable
+            
+
+            $getResults = Get-TargetResource @testParams
+
+            It "returns the expected values from the get method" {
+                $getResults.Ensure | Should Be "Present"
+                $getResults.Version | Should Be "1.0.0.0"
+                $getResults.Deployed | Should Be $true
+            }
+
+            It "returns false from the test method" {
+                Test-TargetResource @testParams | Should Be $false
+            }
+
+            It "uninstalles and removes the solution from the web apps and the farm" {
+                Set-TargetResource @testParams
+
+                Assert-MockCalled Uninstall-SPSolution
+                Assert-MockCalled WaitFor-SolutionJob 
+                Assert-MockCalled Remove-SPSolution 
+            }
         }
 
         Context "The solution isn't installed, and should not be"{
+
+            $testParams = @{
+                Name            = "SomeSolution"
+                LiteralPath     = "\\server\share\file.wsp"
+                Deployed        = $false
+                Ensure          = "Absent"
+                Version         = "0.0.0.0"
+                WebApplications = @()
+            }
+
+            Mock Get-SPSolution { $null } -Verifiable
+
+            $getResults = Get-TargetResource @testParams
+
+            It "returns the expected empty values from the get method" {
+                $getResults.Ensure | Should Be "Absent"
+                $getResults.Version | Should Be "0.0.0.0"
+                $getResults.Deployed | Should Be $false
+            }
+
+            It "returns true from the test method" {
+                Test-TargetResource @testParams | Should Be $true
+            }
         }
 
         Context "The solution is installed, but needs update"{
+
+            $testParams.Version = "1.1.0.0"
+            $testParams.Ensure = "Present"
+
+            Mock Get-SPSolution {
+                $s = [pscustomobject]@{
+                    Deployed                = $true
+                    Properties              = @{ Version = "1.0.0.0" }
+                    DeployedWebApplications = @( [pscustomobject]@{Url="http://app1"}, [pscustomobject]@{Url="http://app2"})
+                    ContainsGlobalAssembly  = $true
+                } 
+                $s | Add-Member -Name Update -MemberType ScriptMethod  -Value { }
+                return $s
+            }        
+
+            $getResults = Get-TargetResource @testParams
+
+            Mock Update-SPSolution { } -Verifiable
+            Mock WaitFor-SolutionJob { } -Verifiable
+            Mock Install-SPFeature { } -Verifiable
+
+            $getResults = Get-TargetResource @testParams
+
+            It "returns the expected values from the get method" {
+                $getResults.Ensure | Should Be "Present"
+                $getResults.Version | Should Be "1.0.0.0"
+                $getResults.Deployed | Should Be $true
+            }
+
+            It "returns false from the test method" {
+                Test-TargetResource @testParams | Should Be $false
+            }
+
+            It "updates the solution in the set method" {
+                Set-TargetResource @testParams
+
+                Assert-MockCalled Update-SPSolution
+                Assert-MockCalled Install-SPFeature
+                Assert-MockCalled WaitFor-SolutionJob 
+            }
         }
 
         Context "The solution is installed, and should be"{
+            
+            $testParams.Version = "1.0.0.0"
+            $testParams.Ensure = "Present"
+
+            Mock Get-SPSolution {
+                return [pscustomobject]@{
+                    Deployed                = $true
+                    Properties              = @{ Version = "1.0.0.0" }
+                    DeployedWebApplications = @( [pscustomobject]@{Url="http://app1"}, [pscustomobject]@{Url="http://app2"})
+                    ContainsGlobalAssembly  = $true
+                }
+            }        
+
+            $getResults = Get-TargetResource @testParams
+
+            It "returns the expected values from the get method" {
+                $getResults.Ensure | Should Be "Present"
+                $getResults.Version | Should Be "1.0.0.0"
+                $getResults.Deployed | Should Be $true
+            }
+
+            It "returns true from the test method" {
+                Test-TargetResource @testParams | Should Be $true
+            }
+        }
+
+        Context "The solution exists but is not deloyed, and needs update"{
+        
+            $testParams.Version = "1.1.0.0"
+            $testParams.Ensure = "Present"
+
+            $solution = [pscustomobject]@{
+                    Deployed                = $false
+                    Properties              = @{ Version = "1.0.0.0" }
+                    DeployedWebApplications = @( [pscustomobject]@{Url="http://app1"}, [pscustomobject]@{Url="http://app2"})
+                    ContainsGlobalAssembly  = $true
+                } 
+            $solution | Add-Member -Name Update -MemberType ScriptMethod  -Value { }
+
+            Mock Get-SPSolution { $solution }      
+
+            $getResults = Get-TargetResource @testParams
+
+            Mock Remove-SPSolution { } -Verifiable
+            Mock Add-SPSolution { $solution } -Verifiable
+
+            Mock Install-SPSolution { } -Verifiable
+            Mock WaitFor-SolutionJob { } -Verifiable
+
+            $getResults = Get-TargetResource @testParams
+
+            It "returns the expected values from the get method" {
+                $getResults.Ensure | Should Be "Present"
+                $getResults.Version | Should Be "1.0.0.0"
+                $getResults.Deployed | Should Be $false
+            }
+
+            It "returns false from the test method" {
+                Test-TargetResource @testParams | Should Be $false
+            }
+
+            It "updates the solution in the set method" {
+                Set-TargetResource @testParams
+
+                Assert-MockCalled Remove-SPSolution
+                Assert-MockCalled Add-SPSolution
+                Assert-MockCalled Install-SPSolution
+                Assert-MockCalled WaitFor-SolutionJob 
+            }
         }
     }   
 }
