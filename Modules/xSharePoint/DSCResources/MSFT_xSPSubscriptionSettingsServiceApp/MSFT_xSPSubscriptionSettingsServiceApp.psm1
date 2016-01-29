c9addc3784cac9cbf4c17e69aa586d6dc39c43e2
Camilo <camilob@gmail.com>
2016-01-29 02:27:11 +0000

    adding proxy and updating tests

--- Modules/xSharePoint/DSCResources/MSFT_xSPSubscriptionSettingsServiceApp/MSFT_xSPSubscriptionSettingsServiceApp.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPSubscriptionSettingsServiceApp/MSFT_xSPSubscriptionSettingsServiceApp.psm1
@@ -63,7 +63,8 @@ function Set-TargetResource
             }
             if ($params.ContainsKey("DatabaseName") -eq $true) { $newParams.Add("DatabaseName", $params.DatabaseName) }
             if ($params.ContainsKey("DatabaseServer") -eq $true) { $newParams.Add("DatabaseServer", $params.DatabaseServer) }
-            New-SPSubscriptionSettingsServiceApplication @newParams
+            $serviceApp = New-SPSubscriptionSettingsServiceApplication @newParams
+            $proxy = New-SPSubscriptionSettingsServiceApplicationProxy -ServiceApplication $serviceApp -DefaultProxyGroup 
         }
     }
     else {
