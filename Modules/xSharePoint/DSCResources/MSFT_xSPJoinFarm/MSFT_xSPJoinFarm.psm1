14423f8eecbf11ef869cd102b74a432a2d073f39
Brian Farnhill <brian@brianfarnhill.com>
2016-01-30 19:01:06 +1100

    Fixed issue with minrole in join farm

--- Modules/xSharePoint/DSCResources/MSFT_xSPJoinFarm/MSFT_xSPJoinFarm.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPJoinFarm/MSFT_xSPJoinFarm.psm1
@@ -76,8 +76,13 @@ function Set-TargetResource
                 Write-Verbose -Message "Detected Version: SharePoint 2013"
             }
             16 {
-                Write-Verbose -Message "Detected Version: SharePoint 2016"
-                $joinFarmArgs.Add("LocalServerRole", "Custom")
+                if ($params.ContainsKey("ServerRole") -eq $true) {
+                    Write-Verbose -Message "Detected Version: SharePoint 2016 - configuring server as $($params.ServerRole)"
+                    $newFarmArgs.Add("LocalServerRole", $params.ServerRole)
+                } else {
+                    Write-Verbose -Message "Detected Version: SharePoint 2016 - no server role provided, configuring server without a specific role"
+                    $newFarmArgs.Add("ServerRoleOptional", $true)
+                }
             }
             Default {
                 throw [Exception] "An unknown version of SharePoint (Major version $_) was detected. Only versions 15 (SharePoint 2013) or 16 (SharePoint 2016) are supported."
