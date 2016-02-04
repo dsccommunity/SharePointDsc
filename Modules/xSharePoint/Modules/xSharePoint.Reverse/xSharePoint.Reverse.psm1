6cc7ef52798dabd91b910dfffc99c6a53b9987c4
Nik Charlebois <nik_charlebois@hotmail.com>
2016-02-04 13:52:36 -0500

    Modifs

--- Modules/xSharePoint/Modules/xSharePoint.Reverse/xSharePoint.Reverse.psm1
+++ Modules/xSharePoint/Modules/xSharePoint.Reverse/xSharePoint.Reverse.psm1
@@ -11,6 +11,7 @@ function Read-SPFarm{
 	{
 		$Script:dscConfigContent += "node " + $spServer.Name + "{`r`n"
 		Read-SPWebApplications
+		Read-SPManagedPaths
 		Read-SPServiceApplicationPools
 		Read-SPSites
 		$Script:dscConfigContent += "}"
@@ -25,22 +26,22 @@ function Read-SPWebApplications
 	foreach($spWebApp in $spWebApplications)
 	{
 		$Script:dscConfigContent += "    xSPWebApplication " + $spWebApp.Name.Replace(" ", "") + "{`r`n"
-		$Script:dscConfigContent += "        Name=" + $spWebApp.Name + "`r`n"
-		$Script:dscConfigContent += "        ApplicationPool=" + $spWebApp.ApplicationPool.Name + "`r`n"
-		$Script:dscConfigContent += "        ApplicationPoolAccount=" + $spWebApplications.ApplicationPool.ProcessAccount + "`r`n"
+		$Script:dscConfigContent += "        Name=`"" + $spWebApp.Name + "`"`r`n"
+		$Script:dscConfigContent += "        ApplicationPool=`"" + $spWebApp.ApplicationPool.Name + "`"`r`n"
+		$Script:dscConfigContent += "        ApplicationPoolAccount=`"" + $spWebApplications.ApplicationPool.ProcessAccount + "`"`r`n"
 
 		$spAuthProvider = Get-SPAuthenticationProvider -WebApplication $spWebApp.Url -Zone "Default"
-		$Script:dscConfigContent += "        AllowAnonymous=" + $spAuthProvider.AllowAnonymous + "`r`n"
+		$Script:dscConfigContent += "        AllowAnonymous=`"" + $spAuthProvider.AllowAnonymous + "`"`r`n"
 
 		if ($spAuthProvider.DisableKerberos -eq $true) { $localAuthMode = "NTLM" } else { $localAuthMode = "Kerberos" }
-		$Script:dscConfigContent += "        AuthenticationMethod=" + $localAuthMode + "`r`n"
+		$Script:dscConfigContent += "        AuthenticationMethod=`"" + $localAuthMode + "`"`r`n"
 
-		$Script:dscConfigContent += "        DatabaseName=" + $spWebApp.ContentDatabases[0].Name + "`r`n"
-		$Script:dscConfigContent += "        DatabaseServer=" + $spWebApp.ContentDatabases[0].Server + "`r`n"
-		$Script:dscConfigContent += "        Url=" + $spWebApp.Url + "`r`n"
-		$Script:dscConfigContent += "        Port=" + (New-Object System.Uri $spWebApp.Url).Port + "`r`n"
-		$Script:dscConfigContent += "        PsDscRunAsCredential=" + $Script:spCentralAdmin.ApplicationPool.ProcessAccount.Name + "`r`n"
-        $Script:dscConfigContent += "        DependsOn=[xSPManagedAccount]WebPoolManagedAccount`r`n"
+		$Script:dscConfigContent += "        DatabaseName=`"" + $spWebApp.ContentDatabases[0].Name + "`"`r`n"
+		$Script:dscConfigContent += "        DatabaseServer=`"" + $spWebApp.ContentDatabases[0].Server + "`"`r`n"
+		$Script:dscConfigContent += "        Url=`"" + $spWebApp.Url + "`"`r`n"
+		$Script:dscConfigContent += "        Port=`"" + (New-Object System.Uri $spWebApp.Url).Port + "`"`r`n"
+		$Script:dscConfigContent += "        PsDscRunAsCredential=`"" + $Script:spCentralAdmin.ApplicationPool.ProcessAccount.Name + "`"`r`n"
+        $Script:dscConfigContent += "        DependsOn=`"[xSPManagedAccount]WebPoolManagedAccount`"`r`n"
 		$Script:dscConfigContent += "    }`r`n"
 	}
 }
@@ -55,7 +56,7 @@ function Read-SPServiceApplicationPools
 		$Script:dscConfigContent += "        Name=`"" + $spServiceAppPool.Name + "`"`r`n"
 		$Script:dscConfigContent += "        ServiceAccount=`"" + $spServiceAppPool.ProcessAccount.Name + "`"`r`n"
 		$Script:dscConfigContent += "        PsDscRunAsCredential=`"" + $Script:spCentralAdmin.ApplicationPool.ProcessAccount.Name + "`"`r`n"
-		$Script:dscConfigContent += "        DependsOn=[xSPCreateFarm]CreateSPFarm`r`n"
+		$Script:dscConfigContent += "        DependsOn=`"[xSPCreateFarm]CreateSPFarm`"`r`n"
 		$Script:dscConfigContent += "    }`r`n"
 	}
 }
@@ -80,8 +81,63 @@ function Read-SPSites
 		$Script:dscConfigContent += "        Template=`"" + $webTemplateName.Name + "`"`r`n"
 		$Script:dscConfigContent += "        Url=`"" + $spSite.Url + "`"`r`n"
 		$Script:dscConfigContent += "        PsDscRunAsCredential=`"" + $Script:spCentralAdmin.ApplicationPool.ProcessAccount.Name + "`"`r`n"
-		$Script:dscConfigContent += "        DependsOn=[xSPWebApplication]" + $spSite.WebApplication.Name.Replace(" ", "") + "`r`n"
+		$Script:dscConfigContent += "        DependsOn=`"[xSPWebApplication]" + $spSite.WebApplication.Name.Replace(" ", "") + "`"`r`n"
 		$Script:dscConfigContent += "    }`r`n"
+	}
+}
+
+function Read-SPManagedPaths
+{
+	$spWebApps = Get-SPWebApplication
+	foreach($spWebApp in $spWebApps)
+	{
+		$spManagedPaths = Get-SPManagedPath -WebApplication $spWebApp.Url
+		foreach($spManagedPath in $spManagedPaths)
+		{
+			if($spManagedPath.Name.Length -gt 0)
+			{
+				$Script:dscConfigContent += "    xSPManagedPath " + $spWebApp.Name.Replace(" ", "") + "Path" + $spManagedPath.Name + "`r`n"
+				$Script:dscConfigContent += "    {`r`n"
+				$Script:dscConfigContent += "        WebAppUrl=`"" + $spWebApp.Url + "`"`r`n"
+				$Script:dscConfigContent += "        RelativeUrl=`"" + $spManagedPath.Name + "`"`r`n"
+				
+				$isExplicit = $false
+				if($spManagedPath.PrefixType -eq "ExplicitInclusion")
+				{
+					$isExplicit = $true
+				}
+				$Script:dscConfigContent += "        Explicit=" + $isExplicit + "`r`n"
+				
+				$Script:dscConfigContent += "        HostHeader=`$false`r`n"
+				$Script:dscConfigContent += "        PsDscRunAsCredential=`"" + $Script:spCentralAdmin.ApplicationPool.ProcessAccount.Name + "`"`r`n"
+				$Script:dscConfigContent += "        DependsOn=`"[xSPWebApplication]" + $spWebApp.Name.Replace(" ", "") + "`"`r`n"
+				$Script:dscConfigContent += "    }`r`n"
+			}			
+		}
+
+		$spManagedPaths = Get-SPManagedPath -HostHeader
+		foreach($spManagedPath in $spManagedPaths)
+		{
+			if($spManagedPath.Name.Length -gt 0)
+			{
+				$Script:dscConfigContent += "    xSPManagedPath " + $spWebApp.Name.Replace(" ", "") + "Path" + $spManagedPath.Name + "`r`n"
+				$Script:dscConfigContent += "    {`r`n"
+				$Script:dscConfigContent += "        WebAppUrl=`"" + $spWebApp.Url + "`"`r`n"
+				$Script:dscConfigContent += "        RelativeUrl=`"" + $spManagedPath.Name + "`"`r`n"
+				
+				$isExplicit = $false
+				if($spManagedPath.PrefixType -eq "ExplicitInclusion")
+				{
+					$isExplicit = $true
+				}
+				$Script:dscConfigContent += "        Explicit=" + $isExplicit + "`r`n"
+				
+				$Script:dscConfigContent += "        HostHeader=`$true`r`n"
+				$Script:dscConfigContent += "        PsDscRunAsCredential=`"" + $Script:spCentralAdmin.ApplicationPool.ProcessAccount.Name + "`"`r`n"
+				$Script:dscConfigContent += "        DependsOn=`"[xSPWebApplication]" + $spWebApp.Name.Replace(" ", "") + "`"`r`n"
+				$Script:dscConfigContent += "    }`r`n"
+			}			
+		}
 	}
 }
 
