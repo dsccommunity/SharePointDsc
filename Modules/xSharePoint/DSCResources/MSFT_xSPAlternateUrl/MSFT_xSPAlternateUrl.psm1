b6ce80751ff61fc075a7939b0b1a7d0125ea9f8f
Brian Farnhill <brian@brianfarnhill.com>
2016-02-11 10:36:15 +1100

    First functional cut of alternate URL resource

--- Modules/xSharePoint/DSCResources/MSFT_xSPAlternateUrl/MSFT_xSPAlternateUrl.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPAlternateUrl/MSFT_xSPAlternateUrl.psm1
@@ -4,11 +4,77 @@ function Get-TargetResource
     [OutputType([System.Collections.Hashtable])]
     param
     (
-        [parameter(Mandatory = $true)]  [System.String] $AppDomain,
-        [parameter(Mandatory = $true)]  [System.String] $Prefix,
+        [parameter(Mandatory = $true)]  [System.String] $WebAppUrl,
+        [parameter(Mandatory = $true)]  [System.String] $Zone,
+        [parameter(Mandatory = $true)]  [System.String] $Url,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
 
     )
 
+    Write-Verbose -Message "Getting Alternate URL for $Zone in $WebAppUrl"
+
+    $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
+        $params = $args[0]
+
+        $aam = Get-SPAlternateURL -WebApplication $params.WebAppUrl -Zone $params.Zone | Select -First 1
+        $url = $null
+        if ($aam -ne $null) {
+            $url = $aam.PublicUrl
+        }
+        
+        return @{
+            WebAppUrl = $params.WebAppUrl
+            Zone = $params.Zone
+            Url = $url
+            InstallAccount = $params.InstallAccount
+        }
+    }
+    return $result
+}
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $WebAppUrl,
+        [parameter(Mandatory = $true)]  [System.String] $Zone,
+        [parameter(Mandatory = $true)]  [System.String] $Url,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+
+    )
+
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+
+    Write-Verbose -Message "Updating app domain settings for $SiteUrl"
+    
+    Invoke-xSharePointCommand -Credential $InstallAccount -Arguments ($PSBoundParameters, $CurrentValues) -ScriptBlock {
+        $params = $args[0]
+        $CurrentValues = $args[1]
+
+        if ([string]::IsNullOrEmpty($CurrentValues.Url)) {
+            New-SPAlternateURL -WebApplication $params.WebAppUrl -Url $params.Url -Zone $params.Zone
+        } else {
+            Set-SPAlternateURL -Identity $params.WebAppUrl -Url $params.Url -Zone $params.Zone
+        }
+    }
+}
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [parameter(Mandatory = $true)]  [System.String] $WebAppUrl,
+        [parameter(Mandatory = $true)]  [System.String] $Zone,
+        [parameter(Mandatory = $true)]  [System.String] $Url,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
+
+    )
+
+    $CurrentValues = Get-TargetResource @PSBoundParameters
+    Write-Verbose -Message "Testing alternate URL configuration"
+    return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues -DesiredValues $PSBoundParameters -ValuesToCheck @("Url") 
 }
 
