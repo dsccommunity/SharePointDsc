893dba280449ad2501ae85a8a42154a099c5b662
Brian Farnhill <brian@brianfarnhill.com>
2016-02-17 19:06:24 +1100

    Added support for default content access account

--- Modules/xSharePoint/DSCResources/MSFT_xSPSearchServiceApp/MSFT_xSPSearchServiceApp.psm1
+++ Modules/xSharePoint/DSCResources/MSFT_xSPSearchServiceApp/MSFT_xSPSearchServiceApp.psm1
@@ -8,6 +8,7 @@ function Get-TargetResource
         [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
         [parameter(Mandatory = $false)] [System.String] $DatabaseServer,
         [parameter(Mandatory = $false)] [System.String] $DatabaseName,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $DefaultContentAccessAccount,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
     )
 
@@ -16,6 +17,11 @@ function Get-TargetResource
     $result = Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
         $params = $args[0]
         
+        [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint")
+        [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SharePoint.Administration")
+        [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Office.Server.Search.Administration")
+        [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Office.Server.Search")
+        [void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.Office.Server") 
 
         $serviceApps = Get-SPServiceApplication -Name $params.Name -ErrorAction SilentlyContinue 
         if ($null -eq $serviceApps) { 
@@ -26,11 +32,19 @@ function Get-TargetResource
         If ($null -eq $serviceApp) { 
             return $null 
         } else {
+            $caWebApp = [Microsoft.SharePoint.Administration.SPAdministrationWebApplication]::Local
+            $caWebApp.Sites[0].Url
+            $s = new-Object Microsoft.SharePoint.SPSite($caWebApp.Sites[0].Url);
+            $c = [Microsoft.Office.Server.Search.Administration.SearchContext]::GetContext($s);
+            $sc = new-Object Microsoft.Office.Server.Search.Administration.Content($c);
+            $defaultAccount = New-Object System.Management.Automation.PSCredential ($sc.DefaultGatheringAccount, (ConvertTo-SecureString "-" -AsPlainText -Force))
+            
             $returnVal =  @{
                 Name = $serviceApp.DisplayName
                 ApplicationPool = $serviceApp.ApplicationPool.Name
                 DatabaseName = $serviceApp.Database.Name
                 DatabaseServer = $serviceApp.Database.Server.Name
+                DefaultContentAccessAccount = $defaultAccount
                 InstallAccount = $params.InstallAccount
             }
             return $returnVal
@@ -49,6 +63,7 @@ function Set-TargetResource
         [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
         [parameter(Mandatory = $false)] [System.String] $DatabaseServer,
         [parameter(Mandatory = $false)] [System.String] $DatabaseName,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $DefaultContentAccessAccount,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
     )
     $result = Get-TargetResource @PSBoundParameters
@@ -66,6 +81,16 @@ function Set-TargetResource
             $app = New-SPEnterpriseSearchServiceApplication @params
             if ($app) {
                 New-SPEnterpriseSearchServiceApplicationProxy -Name "$($params.Name) Proxy" -SearchApplication $app
+                if ($params.ContainsKey("DefaultContentAccessAccount") -eq $true) {
+                    $appPool = Get-SPServiceApplicationPool -Identity $params.ApplicationPool
+                    $setParams = @{
+                        ApplicationPool = $appPool
+                        Identity = $app
+                        DefaultContentAccessAccountName = $params.DefaultContentAccessAccount.UserName
+                        DefaultContentAccessAccountPassword = (ConvertTo-SecureString -String $params.DefaultContentAccessAccount.GetNetworkCredential().Password -AsPlainText -Force)
+                    }
+                    Set-SPEnterpriseSearchServiceApplication @setParams
+                } 
             }
         }
     } else {
@@ -74,10 +99,17 @@ function Set-TargetResource
             Invoke-xSharePointCommand -Credential $InstallAccount -Arguments $PSBoundParameters -ScriptBlock {
                 $params = $args[0]
                 
-
                 $serviceApp = Get-SPServiceApplication -Name $params.Name | Where-Object { $_.TypeName -eq "Search Service Application" }
-                $appPool = Get-SPServiceApplicationPool -Identity $params.ApplicationPool 
-                Set-SPEnterpriseSearchServiceApplication -Identity $serviceApp -ApplicationPool $appPool
+                $appPool = Get-SPServiceApplicationPool -Identity $params.ApplicationPool
+                $setParams = @{
+                    ApplicationPool = $appPool
+                    Identity = $serviceApp
+                }
+                if ($params.ContainsKey("DefaultContentAccessAccount") -eq $true) {
+                    $setParams.Add("DefaultContentAccessAccountName", $params.DefaultContentAccessAccount.UserName)
+                    $setParams.Add("DefaultContentAccessAccountPassword ", (ConvertTo-SecureString -String $params.DefaultContentAccessAccount.GetNetworkCredential().Password -AsPlainText -Force))
+                } 
+                Set-SPEnterpriseSearchServiceApplication @setParams
             }
         }
     }
@@ -94,12 +126,18 @@ function Test-TargetResource
         [parameter(Mandatory = $true)]  [System.String] $ApplicationPool,
         [parameter(Mandatory = $false)] [System.String] $DatabaseServer,
         [parameter(Mandatory = $false)] [System.String] $DatabaseName,
+        [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $DefaultContentAccessAccount,
         [parameter(Mandatory = $false)] [System.Management.Automation.PSCredential] $InstallAccount
     )
 
     $CurrentValues = Get-TargetResource @PSBoundParameters
     Write-Verbose -Message "Testing Search service application '$Name'"
     If ($null -eq $CurrentValues) { return $false }
+    if ($PSBoundParameters.ContainsKey("DefaultContentAccessAccount")) {
+        if ($DefaultContentAccessAccount.UserName -ne $CurrentValues.DefaultContentAccessAccount.UserName) {
+            return $false
+        }
+    }
     return Test-xSharePointSpecificParameters -CurrentValues $CurrentValues -DesiredValues $PSBoundParameters -ValuesToCheck @("ApplicationPool")
 }
 
