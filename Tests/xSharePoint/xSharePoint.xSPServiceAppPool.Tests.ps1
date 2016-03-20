927ed94630aff634f82f9e00f9efedc3a521392b
Brian Farnhill <brian@farnhill.net>
2016-03-21 09:41:41 +1100

    Added ensure to service app pool resource

--- Tests/xSharePoint/xSharePoint.xSPServiceAppPool.Tests.ps1
+++ Tests/xSharePoint/xSharePoint.xSPServiceAppPool.Tests.ps1
@@ -17,6 +17,7 @@ Describe "xSPServiceAppPool" {
         $testParams = @{
             Name = "Service pool"
             ServiceAccount = "DEMO\svcSPServiceApps"
+            Ensure = "Present"
         }
         Import-Module (Join-Path ((Resolve-Path $PSScriptRoot\..\..).Path) "Modules\xSharePoint")
         
@@ -27,12 +28,13 @@ Describe "xSPServiceAppPool" {
         Import-Module $Global:CurrentSharePointStubModule -WarningAction SilentlyContinue 
         Mock New-SPServiceApplicationPool { }
         Mock Set-SPServiceApplicationPool { }
+        Mock Remove-SPServiceApplicationPool { }
 
         Context "A service account pool does not exist but should" {
             Mock Get-SPServiceApplicationPool { return $null }
 
-            It "returns null from the get method" {
-                Get-TargetResource @testParams | Should BeNullOrEmpty
+            It "returns absent from the get method" {
+                (Get-TargetResource @testParams).Ensure | Should Be "Absent"
             }
 
             It "returns false from the test method" {
@@ -69,11 +71,49 @@ Describe "xSPServiceAppPool" {
                 ProcessAccountName = $testParams.ServiceAccount
             }}
 
-            It "retrieves the status from the get method" {
-                Get-TargetResource @testParams | Should Not BeNullOrEmpty
+            It "retrieves present from the get method" {
+                (Get-TargetResource @testParams).Ensure | Should Be "Present"
             }
 
             It "returns true from the test method" {
+                Test-TargetResource @testParams | Should Be $true
+            }
+        }
+        
+        $testParams = @{
+            Name = "Service pool"
+            ServiceAccount = "DEMO\svcSPServiceApps"
+            Ensure = "Absent"
+        }
+        
+        Context "When the service app pool exists but it shouldn't" {
+            Mock Get-SPServiceApplicationPool { return @{
+                Name = $testParams.Name
+                ProcessAccountName = $testParams.ServiceAccount
+            }}
+            
+            It "returns present from the Get method" {
+                (Get-TargetResource @testParams).Ensure | Should Be "Present" 
+            }
+            
+            It "should return false from the test method" {
+                Test-TargetResource @testParams | Should Be $false
+            }
+            
+            It "should remove the service application in the set method" {
+                Set-TargetResource @testParams
+                Assert-MockCalled Remove-SPServiceApplicationPool
+            }
+        }
+        
+        Context "When the service app pool doesn't exist and shouldn't" {
+            Mock Get-SPServiceApplicationPool { return $null }
+            
+            It "returns absent from the Get method" {
+                (Get-TargetResource @testParams).Ensure | Should Be "Absent" 
+            }
+            
+            It "should return false from the test method" {
                 Test-TargetResource @testParams | Should Be $true
             }
         }
