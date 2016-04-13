c247ea8eeaa56bdca18e756f059176b876c807e7
Brian Farnhill <brian@farnhill.net>
2016-04-14 09:49:21 +1000

    Fixing test run for appveyor

--- .vscode/RunPesterTests.ps1
+++ .vscode/RunPesterTests.ps1
@@ -1,6 +1,6 @@
-Import-Module (Join-Path $PSScriptRoot "Tests\xSharePoint.TestHarness.psm1")
+Import-Module (Join-Path $PSScriptRoot "..\Tests\xSharePoint.TestHarness.psm1"  -Resolve)
 
-$DscTestsPath = Join-Path $PSScriptRoot "Modules\DscResource.Tests"
+$DscTestsPath = Join-Path $PSScriptRoot "..\Modules\xSharePoint\DscResource.Tests" -Resolve
 if ((Test-Path $DscTestsPath) -eq $false) {
     Write-Warning "Unable to locate DscResource.Tests repo at '$DscTestsPath', common DSC resource tests will not be executed"
     Invoke-xSharePointTests
