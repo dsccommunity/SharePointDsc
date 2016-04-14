beafb39a86b32fee0940edd6b0e7d136f8f089db
Brian Farnhill <brian@farnhill.net>
2016-04-14 16:33:40 +1000

    Fixed SP build version default

--- Tests/xSharePoint/xSharePoint.xSPFeature.Tests.ps1
+++ Tests/xSharePoint/xSharePoint.xSPFeature.Tests.ps1
@@ -1,6 +1,6 @@
 [CmdletBinding()]
 param(
-    [string] $SharePointCmdletModule = (Join-Path $PSScriptRoot "..\Stubs\SharePoint\15.0.4693.1000\Microsoft.SharePoint.PowerShell.psm1" -Resolve)
+    [string] $SharePointCmdletModule = (Join-Path $PSScriptRoot "..\Stubs\SharePoint\15.0.4805.1000\Microsoft.SharePoint.PowerShell.psm1" -Resolve)
 )
 
 if (!$PSScriptRoot) # $PSScriptRoot is not defined in 2.0
