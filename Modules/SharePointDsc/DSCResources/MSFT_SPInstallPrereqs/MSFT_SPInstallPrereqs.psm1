913701f65fa456f68a5d5ace9bf9e3d11d4da88a
Brian Farnhill <brian@farnhill.net>
2016-06-23 16:52:49 +1000

    Got caught by those damn tabs again - first time in a while!

--- Modules/SharePointDsc/DSCResources/MSFT_SPInstallPrereqs/MSFT_SPInstallPrereqs.psm1
+++ Modules/SharePointDsc/DSCResources/MSFT_SPInstallPrereqs/MSFT_SPInstallPrereqs.psm1
@@ -187,9 +187,9 @@ function Get-TargetResource
     $installedItemsX86 = Get-ItemProperty -Path $x86Path | Select-Object -Property DisplayName
     
     $x64Path = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*"
-	$installedItemsX64 = Get-ItemProperty -Path $x64Path | Select-Object -Property DisplayName
+    $installedItemsX64 = Get-ItemProperty -Path $x64Path | Select-Object -Property DisplayName
 
-	$installedItems = $installedItemsX86 + $installedItemsX64
+    $installedItems = $installedItemsX86 + $installedItemsX64
 
     # Common prereqs
     $prereqsToTest = @(
