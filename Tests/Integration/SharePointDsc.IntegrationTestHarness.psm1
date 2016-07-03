a43bf9647418042638fb6637d35192161303e01d
Brian Farnhill <brian@farnhill.net>
2016-07-03 11:27:05 +1000

    Fixed more path references

--- Tests/Integration/SharePointDsc.IntegrationTestHarness.psm1
+++ Tests/Integration/SharePointDsc.IntegrationTestHarness.psm1
@@ -35,7 +35,7 @@ function Invoke-SPDscIntegrationTest() {
     }
     
     # Run preflight checks
-    $preflightTests = Invoke-Pester "$repoDir\IntegrationTests" -Tag "Preflight" -PassThru
+    $preflightTests = Invoke-Pester "$repoDir\Tests\Integration" -Tag "Preflight" -PassThru
 
     if ($preflightTests.FailedCount -gt 0) {
         throw "Preflight tests have failed!"
@@ -49,13 +49,13 @@ function Invoke-SPDscIntegrationTest() {
     # Execute Pre, main and Post tests for each sequence object
     $testSequence | ForEach-Object {
         Write-Verbose "Starting tests for 'Pre$_'"
-        $testResults.Add("Pre$_", (Invoke-Pester "$repoDir\IntegrationTests" -Tag "Pre$_" -PassThru))
+        $testResults.Add("Pre$_", (Invoke-Pester "$repoDir\Tests\Integration" -Tag "Pre$_" -PassThru))
         
         Write-Verbose "Starting tests for '$_'"
-        $testResults.Add("$_", (Invoke-Pester "$repoDir\IntegrationTests" -Tag "$_" -PassThru))
+        $testResults.Add("$_", (Invoke-Pester "$repoDir\Tests\Integration" -Tag "$_" -PassThru))
         
         Write-Verbose "Starting tests for 'Post$_'"
-        $testResults.Add("Post$_", (Invoke-Pester "$repoDir\IntegrationTests" -Tag "Post$_" -PassThru))
+        $testResults.Add("Post$_", (Invoke-Pester "$repoDir\Tests\Integration" -Tag "Post$_" -PassThru))
     }
     
     # Output the results
