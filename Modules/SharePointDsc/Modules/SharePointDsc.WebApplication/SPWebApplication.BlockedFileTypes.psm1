d3dd97ab1acf0b096b4faef466f73310b6a5caf6
Brian Farnhill <brian@farnhill.net>
2016-07-06 10:30:17 +1000

    Removed plural noun use

--- Modules/SharePointDsc/Modules/SharePointDsc.WebApplication/SPWebApplication.BlockedFileTypes.psm1
+++ Modules/SharePointDsc/Modules/SharePointDsc.WebApplication/SPWebApplication.BlockedFileTypes.psm1
@@ -1,39 +1,57 @@
-function Get-SPDSCWebApplicationBlockedFileTypes {
+function Get-SPDSCWebApplicationBlockedFileTypeConfig
+{
     [CmdletBinding()]
     [OutputType([System.Collections.Hashtable])]
     param(
-        [parameter(Mandatory = $true)] $WebApplication
+        [parameter(Mandatory = $true)] 
+        $WebApplication
     )
     $result = @()
-    $WebApplication.BlockedFileExtensions | ForEach-Object { $result += $_ }
+    $WebApplication.BlockedFileExtensions | ForEach-Object { 
+        $result += $_ 
+    }
     return @{
        Blocked = $result
     }
 }
 
-function Set-SPDSCWebApplicationBlockedFileTypes {
+function Set-SPDSCWebApplicationBlockedFileTypeConfig 
+{
     [CmdletBinding()]
     param(
-        [parameter(Mandatory = $true)] $WebApplication,
-        [parameter(Mandatory = $true)] $Settings
+        [parameter(Mandatory = $true)] 
+        $WebApplication,
+
+        [parameter(Mandatory = $true)] 
+        $Settings
     )
     
-    if (($Settings.ContainsKey("Blocked") -eq $true) -and (($Settings.ContainsKey("EnsureBlocked") -eq $true) -or ($Settings.ContainsKey("EnsureAllowed") -eq $true))) {
-        throw "Blocked file types must use either the 'blocked' property or the 'EnsureBlocked' and/or 'EnsureAllowed' properties, but not both."
+    if (($Settings.ContainsKey("Blocked") -eq $true) `
+            -and (($Settings.ContainsKey("EnsureBlocked") -eq $true) `
+          -or ($Settings.ContainsKey("EnsureAllowed") -eq $true))) 
+    {
+        throw ("Blocked file types must use either the 'blocked' property or the " + `
+               "'EnsureBlocked' and/or 'EnsureAllowed' properties, but not both.")
     }
 
-    if (($Settings.ContainsKey("Blocked") -eq $false) -and ($Settings.ContainsKey("EnsureBlocked") -eq $false) -and ($Settings.ContainsKey("EnsureAllowed") -eq $false)) {
-        throw "Blocked file types must specify at least one property (either 'Blocked, 'EnsureBlocked' or 'EnsureAllowed')"
+    if (($Settings.ContainsKey("Blocked") -eq $false) `
+            -and ($Settings.ContainsKey("EnsureBlocked") -eq $false) `
+            -and ($Settings.ContainsKey("EnsureAllowed") -eq $false)) 
+    {
+        throw ("Blocked file types must specify at least one property (either 'Blocked, " + `
+               "'EnsureBlocked' or 'EnsureAllowed')")
     }
 
-    if($Settings.ContainsKey("Blocked") -eq $true) {
+    if($Settings.ContainsKey("Blocked") -eq $true) 
+    {
         $WebApplication.BlockedFileExtensions.Clear(); 
         $Settings.Blocked | ForEach-Object {
             $WebApplication.BlockedFileExtensions.Add($_.ToLower());
         }
     }
 
-    if($Settings.ContainsKey("EnsureBlocked") -eq $true) {
+    if($Settings.ContainsKey("EnsureBlocked") -eq $true) 
+    {
         $Settings.EnsureBlocked | ForEach-Object {
             if(!$WebApplication.BlockedFileExtensions.Contains($_.ToLower())){
                 $WebApplication.BlockedFileExtensions.Add($_.ToLower());
@@ -41,7 +59,8 @@ function Set-SPDSCWebApplicationBlockedFileTypes {
         }
     }
 
-    if($Settings.ContainsKey("EnsureAllowed") -eq $true) {
+    if($Settings.ContainsKey("EnsureAllowed") -eq $true) 
+    {
         $Settings.EnsureAllowed | ForEach-Object {
             if($WebApplication.BlockedFileExtensions.Contains($_.ToLower())){
                 $WebApplication.BlockedFileExtensions.Remove($_.ToLower());
@@ -50,36 +69,69 @@ function Set-SPDSCWebApplicationBlockedFileTypes {
     }
 }
 
-function Test-SPDSCWebApplicationBlockedFileTypes {
+function Test-SPDSCWebApplicationBlockedFileTypeConfig 
+{
     [CmdletBinding()]
     [OutputType([System.Boolean])]
     param(
-        [parameter(Mandatory = $true)] $CurrentSettings,
-        [parameter(Mandatory = $true)] $DesiredSettings
+        [parameter(Mandatory = $true)] 
+        $CurrentSettings,
+
+        [parameter(Mandatory = $true)] 
+        $DesiredSettings
     )
-    if (($DesiredSettings.ContainsKey("Blocked") -eq $true) -and (($DesiredSettings.ContainsKey("EnsureBlocked") -eq $true) -or ($DesiredSettings.ContainsKey("EnsureAllowed") -eq $true))) {
-        throw "Blocked file types must use either the 'blocked' property or the 'EnsureBlocked' and/or 'EnsureAllowed' properties, but not both."
+    if (($Settings.ContainsKey("Blocked") -eq $true) `
+            -and (($Settings.ContainsKey("EnsureBlocked") -eq $true) `
+          -or ($Settings.ContainsKey("EnsureAllowed") -eq $true))) 
+    {
+        throw ("Blocked file types must use either the 'blocked' property or the " + `
+               "'EnsureBlocked' and/or 'EnsureAllowed' properties, but not both.")
     }
 
-    if (($DesiredSettings.ContainsKey("Blocked") -eq $false) -and ($DesiredSettings.ContainsKey("EnsureBlocked") -eq $false) -and ($DesiredSettings.ContainsKey("EnsureAllowed") -eq $false)) {
-        throw "Blocked file types must specify at least one property (either 'Blocked, 'EnsureBlocked' or 'EnsureAllowed')"
+    if (($Settings.ContainsKey("Blocked") -eq $false) `
+            -and ($Settings.ContainsKey("EnsureBlocked") -eq $false) `
+            -and ($Settings.ContainsKey("EnsureAllowed") -eq $false)) 
+    {
+        throw ("Blocked file types must specify at least one property (either 'Blocked, " + `
+               "'EnsureBlocked' or 'EnsureAllowed')")
     }
 
-    if($DesiredSettings.ContainsKey("Blocked") -eq $true) {
-        $compareResult = Compare-Object -ReferenceObject $CurrentSettings.Blocked -DifferenceObject $DesiredSettings.Blocked
-        if ($null -eq $compareResult) { return $true } else { return $false }
+    if ($DesiredSettings.ContainsKey("Blocked") -eq $true) 
+    {
+        $compareResult = Compare-Object -ReferenceObject $CurrentSettings.Blocked `
+                                        -DifferenceObject $DesiredSettings.Blocked
+        if ($null -eq $compareResult) 
+        { 
+            return $true 
+        } 
+        else 
+        { 
+            return $false 
+        }
     }
     
-    if($DesiredSettings.ContainsKey("EnsureBlocked") -eq $true) {
-        $itemsToAdd = Compare-Object -ReferenceObject $CurrentSettings.Blocked -DifferenceObject $DesiredSettings.EnsureBlocked | Where-Object { $_.SideIndicator -eq "=>"}
-        if ($null -ne $itemsToAdd) { return $false }
+    if ($DesiredSettings.ContainsKey("EnsureBlocked") -eq $true) 
+    {
+        $itemsToAdd = Compare-Object -ReferenceObject $CurrentSettings.Blocked `
+                                     -DifferenceObject $DesiredSettings.EnsureBlocked | Where-Object { 
+                                         $_.SideIndicator -eq "=>"
+                                        }
+        if ($null -ne $itemsToAdd) 
+        { 
+            return $false 
+        }
     }
 
-    if($DesiredSettings.ContainsKey("EnsureAllowed") -eq $true) {
-        $itemsToRemove = Compare-Object -ReferenceObject $CurrentSettings.Blocked -DifferenceObject $DesiredSettings.EnsureAllowed -ExcludeDifferent -IncludeEqual
-        if ($null -ne $itemsToRemove) { return $false } 
+    if ($DesiredSettings.ContainsKey("EnsureAllowed") -eq $true) 
+    {
+        $itemsToRemove = Compare-Object -ReferenceObject $CurrentSettings.Blocked `
+                                        -DifferenceObject $DesiredSettings.EnsureAllowed `
+                                        -ExcludeDifferent -IncludeEqual
+        if ($null -ne $itemsToRemove) 
+        { 
+            return $false 
+        } 
     }
-
     return $true
 }
 
