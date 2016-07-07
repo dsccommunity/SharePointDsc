49c082bcafa57b3df122e3510fe497b85492775f
Brian Farnhill <brian@farnhill.net>
2016-07-07 10:07:57 +1000

    More help files

---
+++ Modules/SharePointDsc/Examples/Resources/SPDistributedCacheService/1-SingleServerCache.ps1
@@ -0,0 +1,25 @@
+<#
+.EXAMPLE
+    This example applies the distributed cache service to the current server,
+    also setting the rules in Windows firewall to allow communication with
+    other cache hosts.
+#>
+
+    Configuration Example 
+    {
+        param(
+            [Parameter(Mandatory = $true)]
+            [PSCredential]
+            $SetupAccount
+        )
+        Import-DscResource -ModuleName SharePointDsc
+
+        SPDistributedCacheService EnableDistributedCache
+        {
+            Name                 = "AppFabricCachingService"
+            CacheSizeInMB        = 8192
+            ServiceAccount       = "DEMO\ServiceAccount"
+            InstallAccount       = $SetupAccount
+            CreateFirewallRules  = $true
+        }
+    }
---
+++ Modules/SharePointDsc/Examples/Resources/SPExcelServiceApp/1-CreateServiceApp.ps1
@@ -0,0 +1,21 @@
+<#
+.EXAMPLE
+    This example shows how to deploy Excel Services to the local SharePoint farm.
+#>
+
+    Configuration Example 
+    {
+        param(
+            [Parameter(Mandatory = $true)]
+            [PSCredential]
+            $SetupAccount
+        )
+        Import-DscResource -ModuleName SharePointDsc
+
+        SPExcelServiceApp ExcelServices
+        {
+            Name            = "Excel Services Service Application"
+            ApplicationPool = "SharePoint Service Applications"
+            InstallAccount  = $SetupAccount
+        }
+    }
