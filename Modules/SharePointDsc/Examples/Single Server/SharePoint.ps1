2adfdec247a8e1d1f0d7997021ead4ea3b4fd327
Brian Farnhill <brian@farnhill.net>
2016-08-03 14:23:25 +1000

    Removed old xWebsite stuff

--- Modules/SharePointDsc/Examples/Single Server/SharePoint.ps1
+++ Modules/SharePointDsc/Examples/Single Server/SharePoint.ps1
@@ -14,22 +14,6 @@ Configuration Example
     node $AllNodes.NodeName
     {        
         #**********************************************************
-        # IIS clean up
-        #
-        # This section removes all default sites and application
-        # pools from IIS as they are not required
-        #**********************************************************
-
-        xWebAppPool RemoveDotNet2Pool         { Name = ".NET v2.0";            Ensure = "Absent" }
-        xWebAppPool RemoveDotNet2ClassicPool  { Name = ".NET v2.0 Classic";    Ensure = "Absent" }
-        xWebAppPool RemoveDotNet45Pool        { Name = ".NET v4.5";            Ensure = "Absent"; }
-        xWebAppPool RemoveDotNet45ClassicPool { Name = ".NET v4.5 Classic";    Ensure = "Absent"; }
-        xWebAppPool RemoveClassicDotNetPool   { Name = "Classic .NET AppPool"; Ensure = "Absent" }
-        xWebAppPool RemoveDefaultAppPool      { Name = "DefaultAppPool";       Ensure = "Absent" }
-        xWebSite    RemoveDefaultWebSite      { Name = "Default Web Site";     Ensure = "Absent"; PhysicalPath = "C:\inetpub\wwwroot" }
-        
-        
-        #**********************************************************
         # Install Binaries
         #
         # This section installs SharePoint and its Prerequisites
