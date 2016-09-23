5a15d7bc4b33d76802c0e2db075900c0f05c4e78
Nik Charlebois <nik_charlebois@hotmail.com>
2016-09-23 16:11:31 -0400

    Mocked Invoke-SPDSCCommand

--- Tests/Unit/SharePointDsc/SharePointDSC.Reverse.Tests.ps1
+++ Tests/Unit/SharePointDsc/SharePointDSC.Reverse.Tests.ps1
@@ -114,6 +114,10 @@ Describe "SharePointDsc.Reverse - SharePoint Build $((Get-Item $SharePointCmdlet
 		# Mocking the Get-SPServiceApplication cmdlet
 		Mock Get-SPServiceApplication { return $null } -ModuleName "SharePointDSC.Reverse"
 
+        Mock Invoke-SPDSCCommand { 
+            return Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $Arguments -NoNewScope
+        }
+
         It "Read information about the farm's configuration" {
 			$modulePath = (Join-Path $RepoRoot "Modules\SharePointDsc\DSCResources\MSFT_SPCreateFarm\MSFT_SPCreateFarm.psm1")
 			$testParams = @{
