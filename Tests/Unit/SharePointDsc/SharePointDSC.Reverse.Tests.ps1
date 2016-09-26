a4a8c694cda6a97afb4a8de9dcd74614cc54cd6a
Nik Charlebois <nik_charlebois@hotmail.com>
2016-09-26 11:47:09 -0400

    New-PSSession Mocked

--- Tests/Unit/SharePointDsc/SharePointDSC.Reverse.Tests.ps1
+++ Tests/Unit/SharePointDsc/SharePointDSC.Reverse.Tests.ps1
@@ -32,10 +32,8 @@ Describe "SharePointDsc.Reverse" {
             return Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $Arguments -NoNewScope
         }
         Mock Invoke-Command { return $null } -ModuleName "SharePointDsc.Reverse"
-
-    Context "Validate Environment Data Extract" {
-        
         Mock New-PSSession { return $null } -ModuleName "SharePointDsc.Reverse"
+    Context "Validate Environment Data Extract" {       
         Mock Get-PSSnapin { return $null } -ModuleName "SharePointDsc.Reverse"
         Mock Add-PSSnapin { return $null } -ModuleName "SharePointDsc.Reverse"
 		Mock Get-Credential { return $null } -ModuleName "SharePointDsc.Reverse"
