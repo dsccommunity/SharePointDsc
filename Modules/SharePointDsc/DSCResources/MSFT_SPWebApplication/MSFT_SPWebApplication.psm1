18e6f295b55f9ed4bc132ea8e04e4a16a9fa1c42
Yorick Kuijs <ykuijs@hotmail.com>
2016-11-16 12:41:53 +0100

    Infer the UseSSL parameter

--- Modules/SharePointDsc/DSCResources/MSFT_SPWebApplication/MSFT_SPWebApplication.psm1
+++ Modules/SharePointDsc/DSCResources/MSFT_SPWebApplication/MSFT_SPWebApplication.psm1
@@ -45,10 +45,6 @@ function Get-TargetResource
         $Port,
 
         [parameter(Mandatory = $false)]
-        [System.Boolean]
-        $UseSSL,
-
-        [parameter(Mandatory = $false)]
         [ValidateSet("NTLM","Kerberos")]
         [System.String] 
         $AuthenticationMethod,
@@ -105,7 +101,6 @@ function Get-TargetResource
             Path = $wa.IisSettings[0].Path
             Port = (New-Object -TypeName System.Uri $wa.Url).Port
             AuthenticationMethod = $localAuthMode
-            UseSSL = (New-Object -TypeName System.Uri $wa.Url).Scheme -eq "https"
             InstallAccount = $params.InstallAccount
             Ensure = "Present"
         }
@@ -158,10 +153,6 @@ function Set-TargetResource
         [parameter(Mandatory = $false)]
         [System.String] 
         $Port,
-
-        [parameter(Mandatory = $false)]
-        [System.Boolean]
-        $UseSSL,
 
         [parameter(Mandatory = $false)]
         [ValidateSet("NTLM","Kerberos")]
@@ -267,10 +258,10 @@ function Set-TargetResource
                 if ($params.ContainsKey("Port") -eq $true) 
                 { 
                     $newWebAppParams.Add("Port", $params.Port) 
-                } 
-                if ($params.ContainsKey("UseSSL") -eq $true) 
+                }
+                if ((New-Object -TypeName System.Uri $params.Url).Scheme -eq "https") 
                 { 
-                    $newWebAppParams.Add("SecureSocketsLayer", $params.UseSSL) 
+                    $newWebAppParams.Add("SecureSocketsLayer", $true) 
                 } 
             
                 New-SPWebApplication @newWebAppParams | Out-Null
@@ -340,10 +331,6 @@ function Test-TargetResource
         [parameter(Mandatory = $false)]
         [System.String] 
         $Port,
-
-        [parameter(Mandatory = $false)]
-        [System.Boolean]
-        $UseSSL,
 
         [parameter(Mandatory = $false)]
         [ValidateSet("NTLM","Kerberos")]
