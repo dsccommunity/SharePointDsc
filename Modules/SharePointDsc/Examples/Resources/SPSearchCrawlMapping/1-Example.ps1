2407877554a5d942dcb6e1578d0d0f6febd00c05
Michael Rees Pullen <mrpullen@outlook.com>
2017-02-28 12:04:54 -0500

    Added Sample of MSFT_SPSearchCrawlMapping

---
+++ Modules/SharePointDsc/Examples/Resources/SPSearchCrawlMapping/1-Example.ps1
@@ -0,0 +1,28 @@
+<#
+.EXAMPLE
+    This example shows how to apply settings to a sepcific URL in search
+#>
+
+    Configuration Example 
+    {
+        param(
+            [Parameter(Mandatory = $true)]
+            [PSCredential]
+            $SetupAccount
+        )
+        Import-DscResource -ModuleName SharePointDsc
+
+        node localhost {
+            SPSearchCrawlMapping IntranetCrawlMapping 
+            {
+                ServiceAppName = "Search Service Application"
+                Url = "http://crawl.sharepoint.com"
+                Target = "http://site.sharepoint.com"
+                Ensure = "Present"
+                InstallAccount = $SetupAccount
+            }
+           
+        }
+    }
+
+
