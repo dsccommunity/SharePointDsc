ae4de6da41dea9fca942c815b491ac2933607916
Michael Rees Pullen <mrpullen@outlook.com>
2017-03-13 14:27:07 -0400

    Added ErrorAction -SilentlyContinue, new tests load the same object. This function will fail if the object already exists, but that's fine as it will already be available.

--- Tests/Unit/SharePointDsc/SharePointDsc.SPSearchResultSource.Tests.ps1
+++ Tests/Unit/SharePointDsc/SharePointDsc.SPSearchResultSource.Tests.ps1
@@ -26,7 +26,7 @@ Describe -Name $Global:SPDscHelper.DescribeHeader -Fixture {
                     public static string Ssa { get { return ""; } }
                 }
             }
-"@
+"@  -ErrorAction SilentlyContinue
 
         # Mocks for all contexts 
         Mock -CommandName Get-SPEnterpriseSearchServiceApplication {
