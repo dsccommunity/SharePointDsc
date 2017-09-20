40a9475937ef369168f9b652aa9c99b865368093
Brian Farnhill <brfarn@microsoft.com>
2017-09-20 16:01:36 +1000

    Work in progress on PS group resource - get method is busted, but set works fine.

---
+++ Modules/SharePointDsc/DSCResources/MSFT_SPProjectServerGroup/MSFT_SPProjectServerGroup.psm1
@@ -0,0 +1,190 @@
+function Get-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Collections.Hashtable])]
+    param
+    (
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Url,
+
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Name,
+
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Description,
+
+        [Parameter(Mandatory = $false)] 
+        [System.Management.Automation.PSCredential] 
+        $InstallAccount
+    )
+
+    Write-Verbose -Message "Getting group settings for '$Name' at '$Url'"
+
+    if ((Get-SPDSCInstalledProductVersion).FileMajorPart -lt 16) 
+    {
+        throw [Exception] ("Support for Project Server in SharePointDsc is only valid for " + `
+                           "SharePoint 2016.")
+    }
+
+    $result = Invoke-SPDSCCommand -Credential $InstallAccount `
+                                  -Arguments @($PSBoundParameters, $PSScriptRoot) `
+                                  -ScriptBlock {
+        $params = $args[0]
+        $scriptRoot = $args[1]
+
+        if ((Get-SPProjectPermissionMode -Url $params.Url) -ne "ProjectServer")
+        {
+            throw [Exception] ("SPProjectServerGroup is design for Project Server permissions " + `
+                               "mode only, and this site is set to SharePoint mode")
+        }
+        
+        $modulePath = "..\..\Modules\SharePointDsc.ProjectServer\ProjectServerConnector.psm1"
+        Import-Module -Name (Join-Path -Path $scriptRoot -ChildPath $modulePath -Resolve)
+
+        $securityService = New-SPDscProjectServerWebService -PwaUrl $params.Url -EndpointName Security
+
+        $script:groupDataSet = $null
+        Use-SPDscProjectServerWebService -Service $securityService -ScriptBlock {
+            $groupInfo  = $securityService.ReadGroupList().SecurityGroups | Where-Object -FilterScript {
+                $_.WSEC_GRP_NAME -eq $groupName
+            }
+
+            if ($null -ne $groupInfo)
+            {
+                $script:groupDataSet = $securityService.ReadGroup($groupInfo.WSEC_GRP_UID)
+            }
+        }
+
+        if ($null -eq $script:groupDataSet)
+        {
+            return @{
+                Url = $params.Url
+                Name = ""
+                Description = ""
+                InstallAccount = $params.InstallAccount
+            }
+        }
+        else
+        {
+            return @{
+                Url = $params.Url
+                Name = $script:groupDataSet.SecurityGroups.WSEC_GRP_NAME
+                Description = $script:groupDataSet.SecurityGroups.WSEC_GRP_DESC
+                InstallAccount = $params.InstallAccount
+            }
+        }
+    }
+    return $result
+}
+
+
+function Set-TargetResource
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Url,
+
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Name,
+
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Description,
+
+        [Parameter(Mandatory = $false)] 
+        [System.Management.Automation.PSCredential] 
+        $InstallAccount
+    )
+
+    Write-Verbose -Message "Setting group settings for '$Name' at '$Url'"
+
+    $currentSettings = Get-TargetResource @PSBoundParameters
+
+    Invoke-SPDSCCommand -Credential $InstallAccount `
+                        -Arguments @($PSBoundParameters, $PSScriptRoot) `
+                        -ScriptBlock {
+
+        $params = $args[0]
+        $scriptRoot = $args[1]
+
+        $modulePath = "..\..\Modules\SharePointDsc.ProjectServer\ProjectServerConnector.psm1"
+        Import-Module -Name (Join-Path -Path $scriptRoot -ChildPath $modulePath -Resolve)
+
+        $securityService = New-SPDscProjectServerWebService -PwaUrl $params.Url -EndpointName Security
+
+        Use-SPDscProjectServerWebService -Service $securityService -ScriptBlock {
+            $groupInfo  = $securityService.ReadGroupList().SecurityGroups | Where-Object -FilterScript {
+                $_.WSEC_GRP_NAME -eq $params.Name
+            }
+
+            if ($null -eq $groupInfo)
+            {
+                # Create a new group with jsut a name so it can be updated with the properties later
+                $newGroupDS = [SvcSecurity.SecurityGroupsDataSet]::new()
+                $newGroup = $newGroupDS.SecurityGroups.NewSecurityGroupsRow()
+                $newGroup.WSEC_GRP_NAME = $params.Name
+                $newGroup.WSEC_GRP_UID = New-Guid
+                $newGroupDS.SecurityGroups.AddSecurityGroupsRow($newGroup)
+                $securityService.CreateGroups($newGroupDS)
+
+                $groupInfo  = $securityService.ReadGroupList().SecurityGroups | Where-Object -FilterScript {
+                    $_.WSEC_GRP_NAME -eq $params.Name
+                }
+            }
+            
+            # Update the existing group
+            $groupDS = $securityService.ReadGroup($groupInfo.WSEC_GRP_UID)
+            $group = $groupDS.SecurityGroups.FindByWSEC_GRP_UID($groupInfo.WSEC_GRP_UID)
+
+            $group.WSEC_GRP_NAME = $params.Name
+            $group.WSEC_GRP_DESC = $params.Description
+
+            $securityService.SetGroups($groupDS)
+        }
+    }
+}
+
+
+function Test-TargetResource
+{
+    [CmdletBinding()]
+    [OutputType([System.Boolean])]
+    param
+    (
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Url,
+
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Name,
+
+        [Parameter(Mandatory = $true)]  
+        [System.String] 
+        $Description,
+
+        [Parameter(Mandatory = $false)] 
+        [System.Management.Automation.PSCredential] 
+        $InstallAccount
+    )
+
+    Write-Verbose -Message "Testing group settings for '$Name' at '$Url'"
+
+    $currentValues = Get-TargetResource @PSBoundParameters
+
+    return Test-SPDscParameterState -CurrentValues $CurrentValues `
+                                    -DesiredValues $PSBoundParameters `
+                                    -ValuesToCheck @(
+                                        "Name",
+                                        "Description"
+                                    )
+}
+
+Export-ModuleMember -Function *-TargetResource
