7e49938e9b8819bb928b338eee545ed67f09588b
Brian Farnhill <brfarn@microsoft.com>
2017-10-09 13:53:05 +1100

    Working cut of SPProjectServerGlobalPermissions

--- Modules/SharePointDsc/DSCResources/MSFT_SPProjectServerGlobalPermissions/MSFT_SPProjectServerGlobalPermissions.psm1
+++ Modules/SharePointDsc/DSCResources/MSFT_SPProjectServerGlobalPermissions/MSFT_SPProjectServerGlobalPermissions.psm1
@@ -136,7 +136,152 @@ function Set-TargetResource
 
     Write-Verbose -Message "Setting global permissions for $EntityType '$EntityName' at '$Url'"
 
-      
+    $currentValues = Get-TargetResource @PSBoundParameters
+
+    $result = Invoke-SPDSCCommand -Credential $InstallAccount `
+                                  -Arguments @($PSBoundParameters, $PSScriptRoot, $currentValues) `
+                                  -ScriptBlock {
+        $params = $args[0]
+        $scriptRoot = $args[1]
+        $currentValues = $args[2]
+
+        $modulePath = "..\..\Modules\SharePointDsc.ProjectServer\ProjectServerConnector.psm1"
+        Import-Module -Name (Join-Path -Path $scriptRoot -ChildPath $modulePath -Resolve)
+
+
+        $allowPermsToAdd = @()
+        $allowPermsToRemove = @()
+        $denyPermsToAdd = @()
+        $denyPermsToRemove = @()
+
+        if ($params.ContainsKey("AllowPermissions") -eq $true)
+        {
+            $allowPermsDifference = Compare-Object -ReferenceObject $currentValues.AllowPermissions `
+                                                   -DifferenceObject $params.AllowPermissions
+
+            $allowPermsDifference | ForEach-Object -Process {
+                $diff = $_
+                switch ($diff.SideIndicator)
+                {
+                    "<=" {
+                        $allowPermsToRemove += $diff.InputObject
+                    }
+                    "=>" {
+                        $allowPermsToAdd += $diff.InputObject
+                    }
+                }
+            }
+        }
+        if ($params.ContainsKey("DenyPermissions") -eq $true)
+        {
+            $denyPermsDifference = Compare-Object -ReferenceObject $currentValues.DenyPermissions `
+                                                   -DifferenceObject $params.DenyPermissions
+
+            $denyPermsDifference | ForEach-Object -Process {
+                $diff = $_
+                switch ($diff.SideIndicator)
+                {
+                    "<=" {
+                        $denyPermsToRemove += $diff.InputObject
+                    }
+                    "=>" {
+                        $denyPermsToAdd += $diff.InputObject
+                    }
+                }
+            }
+        }
+        
+        switch($params.EntityType)
+        {
+            "User" {
+                $resourceService = New-SPDscProjectServerWebService -PwaUrl $params.Url -EndpointName Resource
+                
+                $userId = Get-SPDscProjectServerResourceId -PwaUrl $params.Url -ResourceName $params.EntityName
+                Use-SPDscProjectServerWebService -Service $resourceService -ScriptBlock {
+                    $dataSet = $resourceService.ReadResourceAuthorization($userId)
+
+                    $allowPermsToAdd | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.NewGlobalPermissionsRow()
+                        $row.RES_UID = $userId
+                        $row.WSEC_FEA_ACT_UID = $permissionId
+                        $row.WSEC_ALLOW = $true
+                        $row.WSEC_DENY = $false
+                        $dataSet.GlobalPermissions.AddGlobalPermissionsRow($row)
+                    }
+
+                    $allowPermsToRemove | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.FindByWSEC_FEA_ACT_UIDRES_UID($permissionId, $userId)
+                        $row.Delete()
+                    }
+
+                    $denyPermsToAdd | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.NewGlobalPermissionsRow()
+                        $row.RES_UID = $userId
+                        $row.WSEC_FEA_ACT_UID = $permissionId
+                        $row.WSEC_ALLOW = $false
+                        $row.WSEC_DENY = $true
+                        $dataSet.GlobalPermissions.AddGlobalPermissionsRow($row)
+                    }
+
+                    $denyPermsToRemove | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.FindByWSEC_FEA_ACT_UIDRES_UID($permissionId, $userId)
+                        $row.Delete()
+                    }
+
+                    $resourceService.UpdateResources($dataSet, $false, $true)
+                }
+            }
+            "Group" {
+                $securityService = New-SPDscProjectServerWebService -PwaUrl $params.Url -EndpointName Security
+
+                Use-SPDscProjectServerWebService -Service $securityService -ScriptBlock {
+                    $groupInfo  = $securityService.ReadGroupList().SecurityGroups | Where-Object -FilterScript {
+                        $_.WSEC_GRP_NAME -eq $params.EntityName
+                    }
+                    $dataSet = $securityService.ReadGroup($groupInfo.WSEC_GRP_UID)
+
+                    $allowPermsToAdd | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.NewGlobalPermissionsRow()
+                        $row.WSEC_GRP_UID = $groupInfo.WSEC_GRP_UID
+                        $row.WSEC_FEA_ACT_UID = $permissionId
+                        $row.WSEC_ALLOW = $true
+                        $row.WSEC_DENY = $false
+                        $dataSet.GlobalPermissions.AddGlobalPermissionsRow($row)
+                    }
+
+                    $allowPermsToRemove | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.FindByWSEC_FEA_ACT_UIDWSEC_GRP_UID($permissionId, $groupInfo.WSEC_GRP_UID)
+                        $row.Delete()
+                    }
+
+                    $denyPermsToAdd | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.NewGlobalPermissionsRow()
+                        $row.WSEC_GRP_UID = $groupInfo.WSEC_GRP_UID
+                        $row.WSEC_FEA_ACT_UID = $permissionId
+                        $row.WSEC_ALLOW = $false
+                        $row.WSEC_DENY = $true
+                        $dataSet.GlobalPermissions.AddGlobalPermissionsRow($row)
+                    }
+
+                    $denyPermsToRemove | ForEach-Object -Process {
+                        $permissionId = Get-SPDscProjectServerGlobalPermissionId -PermissionName $_
+                        $row = $dataSet.GlobalPermissions.FindByWSEC_FEA_ACT_UIDWSEC_GRP_UID($permissionId, $groupInfo.WSEC_GRP_UID)
+                        $row.Delete()
+                    }
+
+                    $securityService.SetGroups($dataSet)
+                }
+            }
+        }
+
+    }
 }
 
 
@@ -179,10 +324,8 @@ function Test-TargetResource
     return Test-SPDscParameterState -CurrentValues $CurrentValues `
                                     -DesiredValues $PSBoundParameters `
                                     -ValuesToCheck @(
-                                        "Name",
-                                        "Description",
-                                        "ADGroup",
-                                        "Ensure"
+                                        "AllowPermissions",
+                                        "DenyPermissions"
                                     )
 }
 
