8b0cda8d31e4ebe2b81b222459d51d3a811608e0
Nik Charlebois <nik_charlebois@hotmail.com>
2018-02-16 14:42:13 -0500

    Updated tests for additional code coverage

--- Tests/Unit/SharePointDsc/SharePointDsc.SPDistributedCacheClientSettings.Tests.ps1
+++ Tests/Unit/SharePointDsc/SharePointDsc.SPDistributedCacheClientSettings.Tests.ps1
@@ -43,8 +43,36 @@ Describe -Name $Global:SPDscHelper.DescribeHeader -Fixture {
         Context -Name "Some Distributed Cache Client Settings are Properly Configured" -Fixture {
             $testParams = @{
                 Ensure = "Present"
-                DTCChannelOpenTimeOut = 1500
+                DLTCMaxConnectionsToServer = 5
+                DLTCRequestTimeout = 1000
+                DLTCChannelOpenTimeOut = 1000
+                DVSCMaxConnectionsToServer = 3
+                DVSCRequestTimeout = 1000
+                DVSCChannelOpenTimeOut = 1000
+                DACMaxConnectionsToServer = 3
+                DACRequestTimeout = 1000
+                DACChannelOpenTimeOut = 1000
+                DAFMaxConnectionsToServer = 3
+                DAFRequestTimeout = 1000
+                DAFChannelOpenTimeOut = 1000
+                DAFCMaxConnectionsToServer = 3
+                DAFCRequestTimeout = 1000
+                DAFCChannelOpenTimeOut = 1000
+                DBCMaxConnectionsToServer = 3
+                DBCRequestTimeout = 1000
+                DBCChannelOpenTimeOut = 1000
+                DDCMaxConnectionsToServer = 3
+                DDCRequestTimeout = 1000
+                DDCChannelOpenTimeOut = 1000
                 DSCMaxConnectionsToServer = 5
+                DSCRequestTimeout = 1000
+                DSCChannelOpenTimeOut = 1000
+                DTCMaxConnectionsToServer = 3
+                DTCRequestTimeout = 1000
+                DTCChannelOpenTimeOut = 1500
+                DSTACMaxConnectionsToServer = 3
+                DSTACRequestTimeout = 1000
+                DSTACChannelOpenTimeOut = 1000
             }
 
             It "Should return Ensure equals Present" {
