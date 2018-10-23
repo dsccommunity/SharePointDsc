1f8c449e7fda1fdb2ab9410c9bb821c58335d54d
Nik Charlebois <nik_charlebois@hotmail.com>
2018-10-22 20:03:38 -0400

    Fix if blocks syntaxt

--- Modules/SharePointDsc/DSCResources/MSFT_SPSearchMetadataCategory/MSFT_SPSearchMetadataCategory.psm1
+++ Modules/SharePointDsc/DSCResources/MSFT_SPSearchMetadataCategory/MSFT_SPSearchMetadataCategory.psm1
@@ -131,10 +131,10 @@ function Set-TargetResource
                                                            -SearchApplication $params.ServiceAppName
 
         # The category exists and it shouldn't, delete it;
-        if($params.Ensure -eq "Absent" -and $null -ne $category)
+        if ($params.Ensure -eq "Absent" -and $null -ne $category)
         {
             # If the category we are trying to remove is not empty, throw an error
-            if($category.CrawledPropertyCount -gt 0)
+            if ($category.CrawledPropertyCount -gt 0)
             {
                 throw "Cannot delete Metadata Category $($param.Name) because it contains " + `
                       "Crawled Properties. Please remove all associated Crawled Properties " + `
@@ -146,7 +146,7 @@ function Set-TargetResource
         }
 
         # The category doesn't exist, but should
-        if($params.Ensure -eq "Present" -and $null -eq $category)
+        if ($params.Ensure -eq "Present" -and $null -eq $category)
         {
             $category = New-SPEnterpriseSearchMetadataCategory -Name $params.Name `
                                                                -SearchApplication $params.ServiceAppName
