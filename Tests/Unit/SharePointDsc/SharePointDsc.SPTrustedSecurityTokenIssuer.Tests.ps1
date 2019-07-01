f0c98d5ce72655652b8c97cbb7e1ce23067089f2
Yvan Duhamel <yvandev@outlook.fr>
2019-07-01 11:09:14 +0200

    Update SharePointDsc.SPTrustedSecurityTokenIssuer.Tests.ps1

--- Tests/Unit/SharePointDsc/SharePointDsc.SPTrustedSecurityTokenIssuer.Tests.ps1
+++ Tests/Unit/SharePointDsc/SharePointDsc.SPTrustedSecurityTokenIssuer.Tests.ps1
@@ -28,15 +28,6 @@ Describe -Name $Global:SPDscHelper.DescribeHeader -Fixture {
             return $sptrust
         }
 
-        # Mock -CommandName New-SPTrustedSecurityTokenIssuer {
-        #     return @{
-        #         NameId = "22222222-2222-2222-2222-222222222222@bc23e3e4-5899-4b5d-9cee-27344da5deb5"
-        #         Certificate = [pscustomobject]@{
-        #             Thumbprint = "123ABCFACE123ABCFACE123ABCFACE123ABCFACE"
-        #         }
-        #     }
-        # }
-
         Mock -CommandName Get-ChildItem -MockWith {
             return @(
                 @{
@@ -53,30 +44,201 @@ Describe -Name $Global:SPDscHelper.DescribeHeader -Fixture {
             )
         } -ParameterFilter { $TypeName -eq 'System.Security.Cryptography.X509Certificates.X509Certificate2' }
 
+        Mock -CommandName Get-SPAuthenticationRealm -MockWith {
+            return [Guid]::New("D683A811-9F0E-49BC-8BF1-DC1C5FEC8447")
+        }
+
         # Test contexts
-        Context -Name "The SPTrustedSecurityTokenIssuer does not exist but should, using a signing certificate in the certificate store" -Fixture {
+        Context -Name "The SPTrustedSecurityTokenIssuer does not exist and should be created, using a signing certificate in the certificate store and the realm of the farm" -Fixture {
             $testParams = @{
-                Name                         = "HighTrust"
-                Description                  = "HighTrust fake"
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
                 RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
-                RegisteredIssuerNameRealm = $null
+                RegisteredIssuerNameRealm      = $null
                 SigningCertificateThumbprint   = "123ABCFACE123ABCFACE123ABCFACE123ABCFACE"
-                IsTrustBroker            = $true
-                Ensure                       = "Present"
-            }
-
-            It "Should return absent from the get method" {
-                $getResults = Get-TargetResource @testParams
-                $getResults.Ensure | Should Be "Absent"
-            }
-
-            It "Should return false from the test method" {
-                Test-TargetResource @testParams | Should Be $false
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
             }
 
             It "Should create the SPTrustedSecurityTokenIssuer" {
                 Set-TargetResource @testParams
                 Assert-MockCalled New-SPTrustedSecurityTokenIssuer
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer does not exist and should be created, using a signing certificate in the fire system and the realm of the farm" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                RegisteredIssuerNameRealm      = $null
+                SigningCertificateFilePath     = "F:\Data\DSC\FakeSigning.cer"
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
+            }
+
+            It "Should create the SPTrustedSecurityTokenIssuer" {
+                Set-TargetResource @testParams
+                Assert-MockCalled New-SPTrustedSecurityTokenIssuer
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer does not exist and should be created, using a custom realm" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                RegisteredIssuerNameRealm      = "C47023F3-4109-4C6E-8913-DFC3DBACD8C5"
+                SigningCertificateThumbprint   = "123ABCFACE123ABCFACE123ABCFACE123ABCFACE"
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
+            }
+
+            It "Should create the SPTrustedSecurityTokenIssuer" {
+                Set-TargetResource @testParams
+                Assert-MockCalled New-SPTrustedSecurityTokenIssuer
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer is desired, but both parameters SigningCertificateThumbprint and SigningCertificateFilePath are set while exactly 1 should" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                SigningCertificateThumbprint   = "123ABCFACE123ABCFACE123ABCFACE123ABCFACE"
+                SigningCertificateFilePath     = "F:\Data\DSC\FakeSigning.cer"
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
+            }
+
+            It "should fail validation of signing certificate parameters in the set method" {
+                { Set-TargetResource @testParams } | Should Throw "Cannot use both parameters SigningCertificateThumbprint and SigningCertificateFilePath at the same time."
+            }
+
+            It "should fail validation of signing certificate parameters in the set method" {
+                { Test-TargetResource @testParams } | Should Throw "Cannot use both parameters SigningCertificateThumbprint and SigningCertificateFilePath at the same time."
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer is desired, but both parameters SigningCertificateThumbprint and MetadataEndPoint are set while exactly 1 should" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                SigningCertificateThumbprint   = "123ABCFACE123ABCFACE123ABCFACE123ABCFACE"
+                MetadataEndPoint               = "https://accounts.accesscontrol.windows.net/TENANT.onmicrosoft.com/metadata/json/1"
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
+            }
+
+            It "should fail validation of signing certificate parameters in the set method" {
+                { Set-TargetResource @testParams } | Should Throw "Cannot use both parameters SigningCertificateThumbprint and MetadataEndPoint at the same time."
+            }
+
+            It "should fail validation of signing certificate parameters in the set method" {
+                { Test-TargetResource @testParams } | Should Throw "Cannot use both parameters SigningCertificateThumbprint and MetadataEndPoint at the same time."
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer is desired, but both parameters SigningCertificateFilePath and MetadataEndPoint are set while exactly 1 should" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                SigningCertificateFilePath     = "F:\Data\DSC\FakeSigning.cer"
+                MetadataEndPoint               = "https://accounts.accesscontrol.windows.net/TENANT.onmicrosoft.com/metadata/json/1"
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
+            }
+
+            It "should fail validation of signing certificate parameters in the set method" {
+                { Set-TargetResource @testParams } | Should Throw "Cannot use both parameters SigningCertificateFilePath and MetadataEndPoint at the same time."
+            }
+
+            It "should fail validation of signing certificate parameters in the set method" {
+                { Test-TargetResource @testParams } | Should Throw "Cannot use both parameters SigningCertificateFilePath and MetadataEndPoint at the same time."
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer is desired, but the thumbprint of the signing certificate in parameter SigningCertificateThumbprint is invalid" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                SigningCertificateThumbprint   = "XX123ABCFACEXX"
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
+            }
+
+            It "should fail validation of parameter SigningCertificateThumbprint in the set method" {
+                { Set-TargetResource @testParams } | Should Throw "Parameter SigningCertificateThumbprint does not match valid format '^[A-Fa-f0-9]{40}$'."
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer already exists and should not be changed" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                SigningCertificateThumbprint   = "123ABCFACE123ABCFACE123ABCFACE123ABCFACE"
+                IsTrustBroker                  = $true
+                Ensure                         = "Present"
+            }
+
+            Mock -CommandName Get-SPTrustedSecurityTokenIssuer -MockWith {
+                $sptrust = [pscustomobject]@{
+                    Name                 = $testParams.Name
+                    RegisteredIssuerName = "$($testParams.RegisteredIssuerNameIdentifier)@$(Get-SPAuthenticationRealm)"
+                }
+                return $sptrust
+            }
+
+            It "Should be returned the get method with expected properties" {
+                $getResults = Get-TargetResource @testParams
+                $getResults.Ensure | Should Be "Present"
+                $getResults.RegisteredIssuerNameIdentifier | Should Be "$($testParams.RegisteredIssuerNameIdentifier)"
+                $getResults.RegisteredIssuerNameRealm | Should Be "$(Get-SPAuthenticationRealm)"
+            }
+
+            It "Should return true from the test method" {
+                Test-TargetResource @testParams | Should Be $true
+            }
+        }
+
+        Context -Name "The SPTrustedSecurityTokenIssuer exists and should be removed" -Fixture {
+            $testParams = @{
+                Name                           = "HighTrust"
+                Description                    = "HighTrust fake"
+                RegisteredIssuerNameIdentifier = "22222222-2222-2222-2222-222222222222"
+                SigningCertificateThumbprint   = "123ABCFACE123ABCFACE123ABCFACE123ABCFACE"
+                IsTrustBroker                  = $true
+                Ensure                         = "Absent"
+            }
+
+            Mock -CommandName Get-SPTrustedSecurityTokenIssuer -MockWith {
+                $sptrust = [pscustomobject]@{
+                    Name                 = $testParams.Name
+                    RegisteredIssuerName = "$($testParams.RegisteredIssuerNameIdentifier)@$(Get-SPAuthenticationRealm)"
+                }
+                return $sptrust
+            }
+
+            Mock -CommandName Remove-SPTrustedSecurityTokenIssuer -MockWith { }
+
+            It "Should be returned the get method with expected properties" {
+                $getResults = Get-TargetResource @testParams
+                $getResults.Ensure | Should Be "Present"
+                $getResults.RegisteredIssuerNameIdentifier | Should Be "$($testParams.RegisteredIssuerNameIdentifier)"
+                $getResults.RegisteredIssuerNameRealm | Should Be "$(Get-SPAuthenticationRealm)"
+            }
+
+            It "Should return true from the test method" {
+                Test-TargetResource @testParams | Should Be $false
+            }
+
+            It "Should remove the SPTrustedSecurityTokenIssuer" {
+                Set-TargetResource @testParams
+                Assert-MockCalled Remove-SPTrustedSecurityTokenIssuer
             }
         }
     }
