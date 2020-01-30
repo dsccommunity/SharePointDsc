a1740b25e005ef6cab143691574760586bf435c6
Brian Lalancette <github@lalancette.ca>
2020-01-29 22:16:21 -0500

    Another fix for SPSecureStoreServiceApp & AlwaysOn

--- SharePointDsc/DSCResources/MSFT_SPSecureStoreServiceApp/MSFT_SPSecureStoreServiceApp.psm1
+++ SharePointDsc/DSCResources/MSFT_SPSecureStoreServiceApp/MSFT_SPSecureStoreServiceApp.psm1
@@ -249,8 +249,9 @@ function Set-TargetResource
 
     if ($result.Ensure -eq "Present" -and $Ensure -eq "Present")
     {
-        # Updated check to -notlike with a wildcard as $result.DatabaseServer might not be a FQDN when we are using an Always On AG
+        # Added check for -notlike with a wildcard as $result.DatabaseServer might not be a FQDN when we are using an Always On AG
         if ($PSBoundParameters.ContainsKey("DatabaseServer") -and `
+            ($result.DatabaseServer -ne $DatabaseServer) -and `
             ($DatabaseServer -notlike "$($result.DatabaseServer).*"))
         {
             throw ("Specified database server does not match the actual " + `
@@ -384,9 +385,10 @@ function Test-TargetResource
     Write-Verbose -Message "Current Values: $(Convert-SPDscHashtableToString -Hashtable $CurrentValues)"
     Write-Verbose -Message "Target Values: $(Convert-SPDscHashtableToString -Hashtable $PSBoundParameters)"
 
-    # Updated check to -notlike with a wildcard as $CurrentValues.DatabaseServer might not be a FQDN when we are using an Always On AG
+    # Added check for -notlike with a wildcard as $CurrentValues.DatabaseServer might not be a FQDN when we are using an Always On AG
     if ($PSBoundParameters.ContainsKey("DatabaseServer") -and `
         ($null -ne $CurrentValues.DatabaseServer) -and `
+        ($CurrentValues.DatabaseServer -ne $DatabaseServer) -and `
         ($DatabaseServer -notlike "$($CurrentValues.DatabaseServer).*"))
     {
         Write-Verbose -Message ("Specified database server does not match the actual " + `
