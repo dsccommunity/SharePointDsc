dsccommunity/SharePointDsc

commit 5d914d766663dd7b507fbcf4f7edf5aac016a472

Updated unit tests

SharePointDsc/DSCResources/MSFT_SPTrustedIdentityTokenIssuer/MSFT_SPTrustedIdentityTokenIssuer.psm1
@@ -1,4 +1,4 @@
-﻿function Get-TargetResource
+function Get-TargetResource
 {
     [CmdletBinding()]
     [OutputType([System.Collections.Hashtable])]
@@ -265,9 +265,10 @@ function Set-TargetResource
                     $claimsMappingsArray += $newMapping
                 }
 
-                if (!($claimsMappingsArray | Where-Object -FilterScript {
-                            $_.MappedClaimType -like $params.IdentifierClaim
-                        }))
+                $mappings = ($claimsMappingsArray | Where-Object -FilterScript {
+                    $_.MappedClaimType -like $params.IdentifierClaim
+                })
+                if ($null -eq $mappings)
                 {
                     throw ("IdentifierClaim does not match any claim type specified in ClaimsMappings.")
                 }
@@ -288,9 +289,10 @@ function Set-TargetResource
                     throw "SharePoint failed to create the SPTrustedIdentityTokenIssuer."
                 }
 
-                if ((Get-SPClaimProvider | Where-Object -FilterScript {
-                            $_.DisplayName -eq $params.ClaimProviderName
-                        }))
+                $claimProvider = (Get-SPClaimProvider | Where-Object -FilterScript {
+                    $_.DisplayName -eq $params.ClaimProviderName
+                })
+                if ($null -eq $claimProvider)
                 {
                     $trust.ClaimProviderName = $params.ClaimProviderName
                 }

commit f185b0c1f4abbb195c2fac04ef65c4e6ecc7f1d0

Fix regression introduced in commit 5d914d766663dd7b507fbcf4f7edf5aac016a472

SharePointDsc/DSCResources/MSFT_SPTrustedIdentityTokenIssuer/MSFT_SPTrustedIdentityTokenIssuer.psm1
@@ -299,7 +299,7 @@ function Set-TargetResource
                     $claimProvider = (Get-SPClaimProvider | Where-Object -FilterScript {
                             $_.DisplayName -eq $params.ClaimProviderName
                         })
-                    if ($null -eq $claimProvider)
+                    if ($null -ne $claimProvider)
                     {
                         $trust.ClaimProviderName = $params.ClaimProviderName
                     }