2874ab61016984ab434571d73409fc9907e0d053
Nik Charlebois <nik_charlebois@hotmail.com>
2020-11-14 08:08:53 -0500

    Attempt to fix Unit tests

--- SharePointDsc/Modules/SharePointDSC.Reverse/Import-SPPowerShellSnapIn.ps1
+++ SharePointDsc/Modules/SharePointDSC.Reverse/Import-SPPowerShellSnapIn.ps1
@@ -1,4 +1,6 @@
+$CurrentLocation = Get-Location
 if (!(Get-PSSnapin Microsoft.SharePoint.Powershell -ErrorAction SilentlyContinue))
 {
     Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction 0
 }
+Set-Location $CurrentLocation
