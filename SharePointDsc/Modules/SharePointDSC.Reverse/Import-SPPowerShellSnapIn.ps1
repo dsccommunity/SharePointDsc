733d563a7af06a74f2336825d1ab5551c975d406
Nik Charlebois <nik_charlebois@hotmail.com>
2020-11-16 06:30:18 -0500

    Fix Unit Tests

--- SharePointDsc/Modules/SharePointDSC.Reverse/Import-SPPowerShellSnapIn.ps1
+++ SharePointDsc/Modules/SharePointDSC.Reverse/Import-SPPowerShellSnapIn.ps1
@@ -1,6 +1,6 @@
-$CurrentLocation = Get-Location
+$location = Join-Path -Path $(Get-Location) -ChildPath ".."
 if (!(Get-PSSnapin Microsoft.SharePoint.Powershell -ErrorAction SilentlyContinue))
 {
     Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction 0
 }
-Set-Location $CurrentLocation
+Set-Location $location
