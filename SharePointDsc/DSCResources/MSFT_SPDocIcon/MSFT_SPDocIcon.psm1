cc86184b133e9281b11c26549b6e51a6bbd976e3
Yorick Kuijs <ykuijs@hotmail.com>
2021-01-28 20:46:27 +0100

    Fixing SP2019 unit test

--- SharePointDsc/DSCResources/MSFT_SPDocIcon/MSFT_SPDocIcon.psm1
+++ SharePointDsc/DSCResources/MSFT_SPDocIcon/MSFT_SPDocIcon.psm1
@@ -326,8 +326,9 @@ function Test-TargetResource
 function Export-TargetResource
 {
     $VerbosePreference = "SilentlyContinue"
-    $ParentModuleBase = Get-Module "SharePointDSC" | Select-Object -ExpandProperty Modulebase
-    $module = Join-Path -Path $ParentModuleBase -ChildPath  "\DSCResources\MSFT_SPDocIcon\MSFT_SPDocIcon.psm1" -Resolve
+    $ParentModuleBase = Get-Module "SharePointDsc" -ListAvailable | Select-Object -ExpandProperty Modulebase
+    $module = Join-Path -Path $ParentModuleBase -ChildPath "\DSCResources\MSFT_SPDocIcon\MSFT_SPDocIcon.psm1" -Resolve
+
     $Content = ''
     $params = Get-DSCFakeParameters -ModulePath $module
 
