8cf6d64f25ee5992fb150475eb41fd12391df229
Yorick Kuijs <ykuijs@hotmail.com>
2021-04-06 13:32:08 +0200

    Commiting all other files for this PR

---
+++ SharePointDsc/Examples/Resources/SPUsageDefinition/1-Configure.ps1
@@ -0,0 +1,66 @@
+
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID 80d306fa-8bd4-4a8d-9f7a-bf40df95e661
+
+.AUTHOR DSC Community
+
+.COMPANYNAME DSC Community
+
+.COPYRIGHT DSC Community contributors. All rights reserved.
+
+.TAGS
+
+.LICENSEURI https://github.com/dsccommunity/SharePointDsc/blob/master/LICENSE
+
+.PROJECTURI https://github.com/dsccommunity/SharePointDsc
+
+.ICONURI https://dsccommunity.org/images/DSC_Logo_300p.png
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+Updated author, copyright notice, and URLs.
+
+.PRIVATEDATA
+
+#>
+
+<#
+
+.DESCRIPTION
+ This example shows how to configure the Administrative Actions Usage Definition.
+
+#>
+
+Configuration Example
+{
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [PSCredential]
+        $SetupAccount
+    )
+
+    Import-DscResource -ModuleName SharePointDsc
+
+    node localhost
+    {
+        SPUsageDefinition 'AdministrativeActions'
+        {
+            Name                 = "Administrative Actions"
+            DaysRetained         = 14
+            DaysToKeepUsageFiles = 1
+            MaxTotalSizeInBytes  = 10000000000000
+            Enabled              = $true
+            UsageDatabaseEnabled = $true
+            PSDscRunAsCredential = $SetupAccount
+        }
+    }
+}
