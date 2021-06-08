c6b0fac734c5a48f4ed82a26065227ad55b68736
Yorick Kuijs <ykuijs@hotmail.com>
2021-06-08 14:38:37 +0200

    Corrected tabs

--- SharePointDsc/Modules/SharePointDsc.Search/SPSearchServiceApp.psm1
+++ SharePointDsc/Modules/SharePointDsc.Search/SPSearchServiceApp.psm1
@@ -139,14 +139,14 @@ IF NOT EXISTS(SELECT principal_id FROM sys.database_principals WHERE name = @New
 END
 
 IF NOT EXISTS
-	(SELECT DP1.name AS DatabaseRoleName,
-	   isnull (DP2.name, 'No members') AS DatabaseUserName
-	 FROM sys.database_role_members AS DRM
-	 RIGHT OUTER JOIN sys.database_principals AS DP1
-	   ON DRM.role_principal_id = DP1.principal_id
-	 LEFT OUTER JOIN sys.database_principals AS DP2
-	   ON DRM.member_principal_id = DP2.principal_id
-	WHERE DP1.type = 'R' AND DP2.name = @NewUserName AND DP1.name = 'db_owner')
+    (SELECT DP1.name AS DatabaseRoleName,
+       isnull (DP2.name, 'No members') AS DatabaseUserName
+     FROM sys.database_role_members AS DRM
+     RIGHT OUTER JOIN sys.database_principals AS DP1
+       ON DRM.role_principal_id = DP1.principal_id
+     LEFT OUTER JOIN sys.database_principals AS DP2
+       ON DRM.member_principal_id = DP2.principal_id
+    WHERE DP1.type = 'R' AND DP2.name = @NewUserName AND DP1.name = 'db_owner')
 BEGIN
     DECLARE @roleSQL as varchar(500);
     SET @roleSQL = 'ALTER ROLE db_owner ADD MEMBER [' + @NewUserName + ']';
